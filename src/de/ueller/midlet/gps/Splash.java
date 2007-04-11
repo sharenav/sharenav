644557df683eea8b31f8bb8b1528ee88e3312661
james22 <james22@users.sourceforge.net>
2007-04-11 18:45:00 +0000

    add head

--- src/de/ueller/midlet/gps/Splash.java
+++ src/de/ueller/midlet/gps/Splash.java
@@ -1,3 +1,16 @@
+/*
+ * GpsMid - Copyright (c) 2007 Harald Mueller james22 at users dot sourceforge dot net 
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 package de.ueller.midlet.gps;
 
 import java.io.IOException;
