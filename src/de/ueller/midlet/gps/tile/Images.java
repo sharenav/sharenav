7d1df1dc9ad190658ba1f53b8b54160fa963891b
james22 <james22@users.sourceforge.net>
2007-06-17 16:06:22 +0000

    new target image

--- src/de/ueller/midlet/gps/tile/Images.java
+++ src/de/ueller/midlet/gps/tile/Images.java
@@ -5,13 +5,13 @@ import java.io.IOException;
 import javax.microedition.lcdui.Image;
 
 public class Images {
-	public final Image IMG_PARKING=Image.createImage("/images/parking.png");
-	public final Image IMG_FUEL=Image.createImage("/images/fuel.png");
-	public final Image IMG_SCHOOL=Image.createImage("/images/school.png");
-	public final Image IMG_TELEPHONE=Image.createImage("/images/telephone.png");
-	public final Image IMG_RAILSTATION=Image.createImage("/images/railstation.png");
-	public final Image IMG_AERODROME=Image.createImage("/images/aerodrome.png");
-
+	public final Image IMG_PARKING=Image.createImage("/parking.png");
+	public final Image IMG_FUEL=Image.createImage("/fuel.png");
+	public final Image IMG_SCHOOL=Image.createImage("/school.png");
+	public final Image IMG_TELEPHONE=Image.createImage("/telephone.png");
+	public final Image IMG_RAILSTATION=Image.createImage("/railstation.png");
+	public final Image IMG_AERODROME=Image.createImage("/aerodrome.png");
+	public final Image IMG_TARGET=Image.createImage("/target.png");
 	public Images() throws IOException{
 		
 	}
