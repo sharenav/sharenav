339cbc2506f14daf22c8b828fb41a3bf659df3e0
apmonkey <apmonkey@users.sourceforge.net>
2007-12-17 09:04:18 +0000

    Bugfix: Sending GPX files did not work correclty
    Tried to implement a working version of the obex push of the GPX files
    
    This is the initial version. It should work for small traces, but is very memory intensive.
    I have an improved version, but that depends on other changes that I have not yet checked in

--- src/de/ueller/midlet/gps/data/Gpx.java
+++ src/de/ueller/midlet/gps/data/Gpx.java
@@ -4,11 +4,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 
 import javax.microedition.io.Connector;
-import javax.microedition.io.StreamConnection;
 import javax.microedition.rms.RecordStore;
 import javax.microedition.rms.RecordStoreException;
 import javax.microedition.rms.RecordStoreFullException;
@@ -20,9 +18,11 @@ import javax.obex.Operation;
 import javax.obex.ResponseCodes;
 
 import de.ueller.gps.data.Position;
+import de.ueller.midlet.gps.Logger;
 
 public class Gpx extends PersistEntity implements Runnable {
 	
+	private final static Logger logger=Logger.getInstance(Gpx.class,Logger.DEBUG);
 	private RecordStore database;
 	public int recorded=0;
 	private Thread processorThread=null;
@@ -99,56 +99,67 @@ public class Gpx extends PersistEntity implements Runnable {
 
 	public void run() {
 		try {
-			StreamConnection conn = (StreamConnection) Connector.open(url);
-			ClientSession session = (ClientSession)Connector.open(url);
-	        HeaderSet headers = session.createHeaderSet();
-	        headers.setHeader(HeaderSet.NAME, "export.gpx");
-	        headers.setHeader(HeaderSet.TYPE, "text");
-//	        headers.setHeader(HeaderSet.LENGTH, new Long(imageData.length));
-	        Operation operation;
-	        operation = session.put(headers);
-	        OutputStream outputStream = operation.openOutputStream();
+			//Convert the data stored in the recordStore into a GPX file
+			//Cache it completely in a ByteArrayOutputStream, as we need to
+			//find out the file size of the complete file to send.
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
-//			OutputStream outputStream = conn.openOutputStream();
-			write(outputStream,"<?xml version='1.0' encoding='UTF-8'?>\r\n");
-			write(outputStream,"<gpx version='1.1' creator='GPSMID' xmlns='http://www.topografix.com/GPX/1/1'>\r\n");
-			write(outputStream,"<trk>\r\n");
-			write(outputStream,"<trkseg>\r\n");
-
+			write(baos,"<?xml version='1.0' encoding='UTF-8'?>\r\n");
+			write(baos,"<gpx version='1.1' creator='GPSMID' xmlns='http://www.topografix.com/GPX/1/1'>\r\n");
+			write(baos,"<trk>\r\n");
+			write(baos,"<trkseg>\r\n");
 			byte [] result;
-			for (int i = 1; i <= database.getNumRecords(); i++)
+			for (int i = 1; i <= database.getNumRecords(); i++) {
+				try {
+					if ((result = database.getRecord(i)) != null) {
+						DataInputStream bi = getByteInputStream(result);
+						write(baos,"<trkpt lat='"+ bi.readFloat() + "' lon='" + bi.readFloat() +"' />\r\n");
+					}
+				} catch (RecordStoreException e) {
+					logger.error("RSE: " + e.getMessage());
+				}
+			}
+			write(baos,"</trkseg>\r\n</trk>\r\n</gpx>\r\n");
+			write(baos,"\r\n");
+			database.closeRecordStore();
 
-			  try {
-			    if ((result = database.getRecord(i)) != null) {
-			       DataInputStream bi = getByteInputStream(result);
-			       write(outputStream,"<trkpt lat='"+ bi.readFloat() + "' lon='" + bi.readFloat() +"' />\r\n");
-			    }
-			  } catch (RecordStoreException e) {
-			  }	
-			  write(outputStream,"</trkseg>\r\n</trk>\r\n</gpx>\r\n");
-			  write(outputStream,"\r\n");
-			  outputStream.flush();
-			  outputStream.close();
-			  conn.close();
-			  database.closeRecordStore();
-		      int code = operation.getResponseCode();
-		        if (code == ResponseCodes.OBEX_HTTP_OK) {
-					RecordStore.deleteRecordStore("GPX");
-		        } else {
-		        	System.out.println("problem in File Opex " + code);
-		        }
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			logger.trace("Starting to send a GPX file, about to open a connection to" + url);			
+			ClientSession session = (ClientSession)Connector.open(url);						
+			HeaderSet headers = session.createHeaderSet();	        
+			session.connect(headers);
+			logger.debug("Connected");
+			headers.setHeader(HeaderSet.NAME, "export.gpx");
+			headers.setHeader(HeaderSet.TYPE, "text");
+			headers.setHeader(HeaderSet.LENGTH, new Long(baos.size()));
+			Operation operation;
+			operation = session.put(headers);			
+			OutputStream outputStream = operation.openOutputStream();
+			logger.info("Writing file of length: " + baos.size());
+			outputStream.write(baos.toByteArray());			
+			outputStream.flush();
+			outputStream.close();			
+			session.close();
+			
+			int code = operation.getResponseCode();
+			if (code == ResponseCodes.OBEX_HTTP_OK) {
+				logger.info("Successfully transfered file");
+				RecordStore.deleteRecordStore("GPX");
+			} else {
+				System.out.println("problem in File Opex " + code);
+			}
+		} catch (IOException e) {			
 			e.printStackTrace();
+			logger.error("IOE: " + e.getMessage());			
 		} catch (RecordStoreNotOpenException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e.printStackTrace();			
+			logger.error("RSNOE: " + e.getMessage());
 		} catch (RecordStoreException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e.printStackTrace();			
+			logger.error("RSE: " + e.getMessage());
+		} catch (Exception ee) {
+			ee.printStackTrace();
+			logger.error("E: " + ee.getMessage());
 		}
-
-		
 	}
 
 	private void write(OutputStream outputStream, String string) throws IOException {
