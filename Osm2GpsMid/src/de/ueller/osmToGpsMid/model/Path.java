4974191055dbec31e528f3adf0808dc9efdadc41
james22 <james22@users.sourceforge.net>
2008-03-30 00:28:46 +0000

    *** empty log message ***

--- Osm2GpsMid/src/de/ueller/osmToGpsMid/model/Path.java
+++ Osm2GpsMid/src/de/ueller/osmToGpsMid/model/Path.java
@@ -42,7 +42,7 @@ public class Path {
 		currentSeg.add(n);
 	}
 	
-	public void addNewSegment(){
+	protected void addNewSegment(){
 		if (subPathList == null){
 			subPathList = new LinkedList<SubPath>();
 		}
