4ba91699a5e91f25645a88c10659840cbd2d44f0
james22 <james22@users.sourceforge.net>
2008-08-27 21:45:48 +0000

    North up should be default as long as its not configurable

--- src/de/ueller/midlet/gps/data/ProjFactory.java
+++ src/de/ueller/midlet/gps/data/ProjFactory.java
@@ -5,7 +5,7 @@ public final class ProjFactory {
 	public static final byte NORTH_UP=0;
 	public static final byte MOVE_UP=1;
 	
-	public static byte type=MOVE_UP;
+	public static byte type=NORTH_UP;
 	
 	public static Projection getInstance(Node center, int upDir, float scale, int width, int height){
 		switch (type){
