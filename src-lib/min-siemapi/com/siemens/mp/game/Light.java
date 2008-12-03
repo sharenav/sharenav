be44a1c42186eaa26593072f94ee8766afb392c2
sk750 <sk750@users.sourceforge.net>
2008-12-03 22:09:27 +0000

    add min-siemapi.jar
    This contains api declarations to keep backlight on and vibrate using the Siemens API.

---
+++ src-lib/min-siemapi/com/siemens/mp/game/Light.java
@@ -0,0 +1,10 @@
+package com.siemens.mp.game;
+
+public class Light {
+    public static void setLightOn()
+    {
+    }
+    
+    public static void setLightOff()
+    {}
+}
---
+++ src-lib/min-siemapi/com/siemens/mp/game/Vibrator.java
@@ -0,0 +1,10 @@
+package com.siemens.mp.game;
+
+public class Vibrator {
+    public static void startVibrator()
+    {}
+    public static void stopVibrator()
+    {}
+    public static void triggerVibrator(int duration)
+    {}
+}
