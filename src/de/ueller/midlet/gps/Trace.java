e585064cefebe0ef6fa6777249e5b9b8f8c21ee5
apmonkey <apmonkey@users.sourceforge.net>
2009-01-08 09:27:27 +0000

    Reverting last commit of sk750 to help in merge conflicts
    I had some time on the plane yesterday, so I have begun to refactor some code to make keyboard short cuts configurable.
    
    Unfortunately as I hadn't mentioned it to anyone, these now conflict with the changes sk750 did at the same time. I think
    it is easier if I revert this commit, then commit my changes and the try and fix the issue of this patch in a new way.
    
    In case this causes more headaches for you, as you had other uncommited changes too, then feel free to revert everything back again
    and we will have to see how to go about this.
    
    Sorry for this.

--- src/de/ueller/midlet/gps/Trace.java
+++ src/de/ueller/midlet/gps/Trace.java
@@ -158,8 +158,6 @@ public class Trace extends Canvas implements CommandListener, LocationMsgReceive
 	private static int pressedKeyCode = 0;
 	private static volatile long releasedKeyCode = 0;
 	private static int ignoreKeyCode = 0;
-	private static int lastGameKeyCode = 0;
-	private static int lastGameAction = 0;
 	
 	// position display was touched last time
 	private static int touchX = 0;
@@ -1978,34 +1976,14 @@ public class Trace extends Canvas implements CommandListener, LocationMsgReceive
 	private int getManualRotationFromKey(int keyCode) {
 		int courseDiff=0;
 		if (manualRotationMode) {
-			if (getGameActionIfNotOverloaded(keyCode) == LEFT) {		
+			if (this.getGameAction(keyCode) == LEFT) {		
 				courseDiff=-5;
-			} else if (getGameActionIfNotOverloaded(keyCode) == RIGHT) {		
+			} else if (this.getGameAction(keyCode) == RIGHT) {		
 				courseDiff=5;
 			}
 		}
 		return courseDiff;
 	}
-	
-	private int getGameActionIfNotOverloaded(int keyCode) {		 
-		// speed up repeatedly asked keyCode by returning remembered gameAction
-		if (lastGameKeyCode == keyCode) {
-			return lastGameAction;
-		} else if  (
-			repeatableKeyPressCommand.get(keyCode) != null ||
-			singleKeyPressCommand.get(keyCode) != null ||
-			longKeyPressCommand.get(keyCode) != null ||
-			doubleKeyPressCommand.get(keyCode) != null
-		 ) {
-			// filter out game keys that are used for other commands
-			lastGameAction=0;
-		} else {
-			lastGameAction = this.getGameAction(keyCode);
-		}
-		lastGameKeyCode = keyCode;
-		return lastGameAction;
-	}
-		 
 	
 	protected void keyRepeated(int keyCode) {
 		// strange seem to be working in emulator only with this debug line
@@ -2016,7 +1994,7 @@ public class Trace extends Canvas implements CommandListener, LocationMsgReceive
 			logger.debug("key ignored " + keyCode);
 			return;
 		}
-		int gameActionCode = getGameActionIfNotOverloaded(keyCode);
+		int gameActionCode = this.getGameAction(keyCode);
 		if ((gameActionCode == UP) || (gameActionCode == DOWN) ||
 				(gameActionCode == RIGHT) || (gameActionCode == LEFT)) {
 			keyPressed(keyCode);
@@ -2089,9 +2067,8 @@ public class Trace extends Canvas implements CommandListener, LocationMsgReceive
 								//#debug debug
 								logger.debug("single key pressed " + keyCode +  " delayed executing command " + singleC);
 								// key was not pressed again within double press time
-								releasedKeyCode = 0;
-								// so this is a single press key
 								commandAction(singleC,(Displayable) null);
+								releasedKeyCode = 0;
 								repaint(0, 0, getWidth(), getHeight());
 							}
 						}			
@@ -2137,13 +2114,13 @@ public class Trace extends Canvas implements CommandListener, LocationMsgReceive
 					course += 360;
 				}
 			}
-		} else if (getGameActionIfNotOverloaded(keyCode) == UP) {
+		} else if (this.getGameAction(keyCode) == UP) {
 			panVertical = -2;
-		} else if (getGameActionIfNotOverloaded(keyCode) == DOWN) {	
+		} else if (this.getGameAction(keyCode) == DOWN) {	
 			panVertical = 2;
-		} else if (getGameActionIfNotOverloaded(keyCode) == LEFT) {		
+		} else if (this.getGameAction(keyCode) == LEFT) {		
 			panHorizontal = -2;
-		} else if (getGameActionIfNotOverloaded(keyCode) == RIGHT) {		
+		} else if (this.getGameAction(keyCode) == RIGHT) {		
 			panHorizontal = 2;
 		}
 		if (	(imageCollector != null) &&
