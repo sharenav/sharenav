b129dec81db580c093b2c716b7fc310ffc18db2a
sk750 <sk750@users.sourceforge.net>
2009-02-14 15:16:54 +0000

    BugFix: set speeding flag not if maxSpeed of the way is 0

--- src/de/ueller/midlet/gps/Trace.java
+++ src/de/ueller/midlet/gps/Trace.java
@@ -1198,7 +1198,7 @@ Runnable , GpsMidDisplayable{
 			int maxSpeed = 0;
 			if (actualWay != null) {
 				maxSpeed = actualWay.getMaxSpeed();
-				if (speed > maxSpeed) {
+				if (maxSpeed != 0 && speed > maxSpeed) {
 					speeding = true;
 				}
 			}
