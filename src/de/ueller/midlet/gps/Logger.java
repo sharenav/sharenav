6f8c6fd6574d4931722e97315b6c3ffe23e7ab11
mbaeurle <mbaeurle@users.sourceforge.net>
2009-03-13 01:27:26 +0000

    -Alerts showing exceptions should not have a timeout. They can occur at any time and we find them important enough to display to the user, so we can't make them disappear as if there was nothing after 5 s. -Exception alert text had an opening brace but no closing. -Code cosmetics: white spaces.

--- src/de/ueller/midlet/gps/Logger.java
+++ src/de/ueller/midlet/gps/Logger.java
@@ -2,7 +2,6 @@ package de.ueller.midlet.gps;
 
 import de.ueller.gps.data.Configuration;
 import javax.microedition.lcdui.Alert;
-import javax.microedition.lcdui.Display;
 
 /*
  * GpsMid - Copyright (c) 2007 Harald Mueller james22 at users dot sourceforge dot net 
@@ -19,86 +18,99 @@ public class Logger {
 	public final static int TRACE = 5;
 	private static GpsMid app;
 	private String source;
-	private int level=ERROR;
+	private int level = ERROR;
 	private static boolean infoEnabled;
 	private static boolean debugEnabled;
 	private static boolean traceEnabled;
-	public Logger(GpsMid app){
-		Logger.app=app;
+
+	public Logger(GpsMid app) {
+		Logger.app = app;
 		this.source = getClassName(app.getClass());
 	}
-	public Logger(Class c){
-		this.source=getClassName(c);		
+
+	public Logger(Class c) {
+		this.source = getClassName(c);		
 	}
-	public Logger(Class c,int level){
-		this.source=getClassName(c);
-		this.level=level;		
+
+	public Logger(Class c,int level) {
+		this.source = getClassName(c);
+		this.level = level;		
 	}
-	public static Logger getInstance(Class c){
-		if (app == null){
+
+	public static Logger getInstance(Class c) {
+		if (app == null) {
 			return null;
 //			throw new Error("not initialized");
 		}
 		return new Logger(c);
 	}
-	public static Logger getInstance(Class c,int level){
-		if (app == null){
+
+	public static Logger getInstance(Class c, int level){
+		if (app == null) {
 			return null;
 //			throw new Error("not initialized");
 		}
 		return new Logger(c,level);
 	}
+
 	public void fatal(String msg){
 		if (level >= FATAL) {
-			app.log("F["+source + msg);			
+			app.log("F[" + source + msg);			
 			GpsMid.getInstance().alert("Fatal", msg, Alert.FOREVER);
 		}
 	}
+
 	public void error(String msg){		
 		error(msg, false);
 	}
+
 	public void error(String msg, boolean silent){		
 		if (level >= ERROR) {
-			app.log("E["+source + msg);
-			if(!silent) {
-				GpsMid.getInstance().alert("Error", msg, 5000);			
+			app.log("E[" + source + msg);
+			if (!silent) {
+				GpsMid.getInstance().alert("Error", msg, Alert.FOREVER);			
 			}
 		}		
 	}
+
 	public void exception(String msg, Exception e) {		
-		error(msg + " (" + e + ": " + e.getMessage());
+		error(msg + ": " + e + ": " + e.getMessage());
 		e.printStackTrace();		
 	}
+
 	public void silentexception(String msg, Exception e) {		
-		error(msg + " (" + e + ": " + e.getMessage(), true);
+		error(msg + ": " + e + ": " + e.getMessage(), true);
 		e.printStackTrace();		
 	}
-	public void info(String msg){
+
+	public void info(String msg) {
 		//#mdebug info
 		if (level >= INFO && infoEnabled) {
-			app.log("I["+source + msg);
+			app.log("I[" + source + msg);
 		}
 		//#enddebug
 	}
-	public void debug(String msg){
+
+	public void debug(String msg) {
 		//#mdebug debug
 		if (level >= DEBUG && debugEnabled) {
-			app.log("D["+source + msg);
+			app.log("D[" + source + msg);
 		}
 		//#enddebug
 	}
-	public void trace(String msg){
+
+	public void trace(String msg) {
 		//#mdebug debug
 		if (level >= TRACE && traceEnabled) {
-			app.log("T["+source + msg);
+			app.log("T[" + source + msg);
 		}
 		//#enddebug
 	}
 	
 	private static String getClassName(Class c) {
 		if (c != null) {
-			String n=c.getName();
-			return n.substring(n.lastIndexOf('.')+1, n.length()) + "] ";
+			String n = c.getName();
+			return n.substring(n.lastIndexOf('.') + 1, n.length()) + "] ";
 		} else {
 			return "";
 		}
