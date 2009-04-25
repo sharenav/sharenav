37fd9c555ba6565b3ed18ff33e17079ef6df69f7
mbaeurle <mbaeurle@users.sourceforge.net>
2009-04-25 23:15:09 +0000

    Code cosmetics: Wrong indentation, white spaces, single-line blocks.

--- src/de/ueller/midlet/gps/tile/SearchNames.java
+++ src/de/ueller/midlet/gps/tile/SearchNames.java
@@ -1,7 +1,7 @@
 package de.ueller.midlet.gps.tile;
 /*
  * GpsMid - Copyright (c) 2007 Harald Mueller james22 at users dot sourceforge dot net
- * 			Copyright (c) 2008 Kai Krueger apm at users dot sourceforge dot net 
+ * 			Copyright (c) 2008 Kai Krueger apmonkey at users dot sourceforge dot net 
  * See Copying
  */
 
@@ -20,16 +20,17 @@ import de.ueller.midlet.gps.Trace;
 import de.ueller.midlet.gps.names.Names;
 import de.ueller.midlet.gps.names.NumberCanon;
 
-public class SearchNames implements Runnable{
+public class SearchNames implements Runnable {
 
 	private Thread processorThread;
-	private int foundEntries=0;
-	private boolean stopSearch=false;
+	private int foundEntries = 0;
+	private boolean stopSearch = false;
 	private String search;
 	private final GuiSearch gui;
-	private boolean newSearch=false;
-	private boolean appendRes=false;
-	protected static final Logger logger = Logger.getInstance(SearchNames.class,Logger.TRACE);
+	private boolean newSearch = false;
+	private boolean appendRes = false;
+	protected static final Logger logger = 
+		Logger.getInstance(SearchNames.class, Logger.TRACE);
 
 	public SearchNames(GuiSearch gui) {
 		super();
@@ -37,31 +38,30 @@ public class SearchNames implements Runnable{
 	}
 
 	public void run() {
-	
 	    try {
-		while (newSearch) {
-		    doSearch(search);
-		    // refresch display to give change to fetch the names
-		    for (int i=8;i!=0;i--){
-			try {
-			    synchronized (this) {
-				wait(300);						
+			while (newSearch) {
+			    doSearch(search);
+			    // refresh display to give chance to fetch the names
+			    for (int i = 8; i != 0; i--) {
+					try {
+					    synchronized (this) {
+					    	wait(300);						
+					    }
+					    if (stopSearch) {
+							break;
+					    } else {
+							gui.triggerRepaint();
+					    }
+					} catch (InterruptedException e) {
+					}
 			    }
-			    if (stopSearch){
-					break;
-			    } else {
-					gui.triggerRepaint();
-			    }
-			} catch (InterruptedException e) {
 			}
-		    }
-		}
 	    } catch (OutOfMemoryError oome ) {
-		logger.fatal("SearchNames thread crashed as out of memory: " + oome.getMessage());
-		oome.printStackTrace();
+	    	logger.fatal("SearchNames thread crashed as out of memory: " + oome.getMessage());
+	    	oome.printStackTrace();
 	    } catch (Exception e) {
-		logger.fatal("SearchNames thread crashed unexpectadly with error " +  e.getMessage());
-		e.printStackTrace();
+	    	logger.fatal("SearchNames thread crashed unexpectedly with error " +  e.getMessage());
+	    	e.printStackTrace();
 	    }		
 	}
 	
@@ -69,12 +69,12 @@ public class SearchNames implements Runnable{
 	private void doSearch(String search) throws IOException {
 		try {
 			synchronized(this) {
-				stopSearch=false;
+				stopSearch = false;
 				if (newSearch) {
 					if (!appendRes) {
 						gui.clearList();
 					}
-					newSearch=false;
+					newSearch = false;
 				}
 			}
 			
@@ -123,12 +123,12 @@ public class SearchNames implements Runnable{
 			
 			//#debug
 			logger.info("Searching for " + search);
-			String fn=search.substring(0,2);
-			String compare=search.substring(2);
-			StringBuffer current=new StringBuffer();
+			String fn = search.substring(0,2);
+			String compare = search.substring(2);
+			StringBuffer current = new StringBuffer();
 			
-			String fileName = "/s"+fn+".d";
-//			System.out.println("open " +fileName);
+			String fileName = "/s" + fn + ".d";
+//			System.out.println("open " + fileName);
 			InputStream stream;
 			try {
 				 stream = Configuration.getMapResource(fileName);
@@ -146,18 +146,17 @@ public class SearchNames implements Runnable{
 				 */
 				return;				
 			}			
-			DataInputStream ds=new DataInputStream(stream);
+			DataInputStream ds = new DataInputStream(stream);
 			
-			int pos=0;
+			int pos = 0;
 			int type = 0;
 			/**
 			 * InputStream.available doesn't seem to reliably give a correct value
 			 * depending on what type of InputStream we actually get.
 			 * Instead we will continue reading until we receive a EOFexpetion.
 			 */
-			while (true){
-				
-				if (stopSearch){
+			while (true) {
+				if (stopSearch) {
 					ds.close();					
 					return;
 				}
@@ -178,67 +177,68 @@ public class SearchNames implements Runnable{
 				 * 000xxxxx delta 
 				 */				
 				//System.out.println("type = " + type);
-				int sign=1;
+				int sign = 1;
 				if ((type & 0x80) != 0) {
 					sign = -1;
 				}
 				int delta = (type & 0x1f) * sign;				
 				//System.out.println("type = " + type);
-				int entryType=(type & 0x60);
-				if (delta > Byte.MAX_VALUE)
+				int entryType = (type & 0x60);
+				if (delta > Byte.MAX_VALUE) {
 					delta -= Byte.MAX_VALUE;
-				pos+=delta;				
+				}
+				pos += delta;				
 				current.setLength(pos);
-//				System.out.println("pos=" + pos + "  delta="+delta);
-				long value=0;
+//				System.out.println("pos=" + pos + "  delta=" + delta);
+				long value = 0;
 				switch (entryType){
 				case 0:
-					value=ds.readByte();
+					value = ds.readByte();
 //					System.out.println("read byte");
 					break;
 				case 0x20:
-					value=ds.readShort();
+					value = ds.readShort();
 //					System.out.println("read short");
 					break;
 				case 0x40:
-					value=ds.readInt();
+					value = ds.readInt();
 //					System.out.println("read int");
 					break;
 				case 0x60:
-					value=ds.readLong();
+					value = ds.readLong();
 //					System.out.println("read long");
 					break;
 				}
-				current.append(""+value);
+				current.append("" + value);
 //				System.out.println("test " + current);
 				int idx = Names.readNameIdx(ds);
-				if (!current.toString().startsWith(compare)){
-					idx=-1;
+				if (!current.toString().startsWith(compare)) {
+					idx = -1;
 				}
 				type = ds.readByte();
 								
-				while (type != 0){					
-					if (stopSearch){
+				while (type != 0) {					
+					if (stopSearch) {
 						ds.close();						
 						return;
 					}					
-					byte isInCount=ds.readByte();
-					int[] isInArray=null;
-					if (isInCount > 0 ){
-						isInArray=new int[isInCount];
-						for (int jj=isInCount;jj--!=0;){
-							isInArray[jj]= Names.readNameIdx(ds);							
+					byte isInCount = ds.readByte();
+					int[] isInArray = null;
+					if (isInCount > 0 ) {
+						isInArray = new int[isInCount];
+						for (int jj = isInCount; jj-- != 0;) {
+							isInArray[jj] = Names.readNameIdx(ds);							
 						}
 					}
-					float lat=ds.readFloat();
-					float lon=ds.readFloat();
-					if (idx != -1){
-						SearchResult sr=new SearchResult();
-						sr.nameIdx=idx;
-						sr.type=(byte) type;
-						sr.lat=lat;
-						sr.lon=lon;
-						sr.nearBy=isInArray;
+					float lat = ds.readFloat();
+					float lon = ds.readFloat();
+					if (idx != -1) {
+						SearchResult sr = new SearchResult();
+						sr.nameIdx = idx;
+						sr.type = (byte) type;
+						sr.lat = lat;
+						sr.lon = lon;
+						sr.nearBy = isInArray;
 						// when there are no nearBys we add the distance so it will be shown by the GUI
 						if (isInArray == null) {
 							gui.addDistanceToSearchResult(sr);
@@ -247,7 +247,7 @@ public class SearchNames implements Runnable{
 							if (!appendRes) {						
 								gui.clearList();
 							}
-							newSearch=false;
+							newSearch = false;
 						}
 						gui.addResult(sr);
 						foundEntries++;
@@ -260,15 +260,15 @@ public class SearchNames implements Runnable{
 						//System.out.println("found " + current +"(" + idx + ") type=" + type);
 					}
 					type = ds.readByte();
-				}
-			}			
+				} // while (type != 0)
+			} // while (true)
 		} catch (NullPointerException e) {
 			logger.exception("Null pointer exception in SearchNames: ", e);			
 		}
 	}
 	
-	public void shutdown(){
-		stopSearch=true;
+	public void shutdown() {
+		stopSearch = true;
 	}
 	
 	/**
@@ -278,10 +278,10 @@ public class SearchNames implements Runnable{
 	 */
 	public synchronized void appendSearchBlocking(String search) {
 		logger.info("search for  " + search);
-		stopSearch=true;
-		newSearch=true;
+		stopSearch = true;
+		newSearch = true;
 		appendRes = true;
-		foundEntries=0;
+		foundEntries = 0;
 		try {
 			doSearch(search);
 		} catch (IOException ioe) {
@@ -293,23 +293,21 @@ public class SearchNames implements Runnable{
 	 * search for a String and create a new search Thread if necessary
 	 * @param search
 	 */
-	public synchronized void search(String search){
+	public synchronized void search(String search) {
 		//#debug
 		logger.info("search for  " + search);
-		stopSearch=true;
-		newSearch=true;
+		stopSearch = true;
+		newSearch = true;
 		appendRes = false;
-		foundEntries=0;
-		this.search=search;
+		foundEntries = 0;
+		this.search = search;
 		if (processorThread == null || !processorThread.isAlive()) {
 			processorThread = new Thread(this);
-			processorThread.setPriority(Thread.MIN_PRIORITY+1);
+			processorThread.setPriority(Thread.MIN_PRIORITY + 1);
 			processorThread.start();
 			//#debug info
 			logger.info("started search thread");
 		}		
 	}
 
-
 }
-
