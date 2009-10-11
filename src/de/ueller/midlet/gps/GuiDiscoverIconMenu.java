ee70eefde13030abe6f0d9ef0f2198b7d3a65c0f
apmonkey <apmonkey@users.sourceforge.net>
2009-10-11 12:44:12 +0000

    [I18N preparation] Mark strings in GuiDiscoverIconMenu for internationalisation

--- src/de/ueller/midlet/gps/GuiDiscoverIconMenu.java
+++ src/de/ueller/midlet/gps/GuiDiscoverIconMenu.java
@@ -18,27 +18,27 @@ public class GuiDiscoverIconMenu extends IconMenuWithPagesGUI {
 	
 		IconMenuPage mp;
 		// Basic
-		mp = createAndAddMenuPage(" Basic ", 3, 3);
-			mp.createAndAddIcon("Location Receiver", "is_loc", GuiDiscover.MENU_ITEM_LOCATION);
-			mp.createAndAddIcon("Display", "is_display", GuiDiscover.MENU_ITEM_DISP_OPT);
-			mp.createAndAddIcon("Sounds & Alerts", "is_sound", GuiDiscover.MENU_ITEM_SOUNDS_OPT);
+		mp = createAndAddMenuPage(" Basic "/*i:Basic*/, 3, 3);
+			mp.createAndAddIcon("Location Receiver"/*i:LocReceiver*/, "is_loc", GuiDiscover.MENU_ITEM_LOCATION);
+			mp.createAndAddIcon("Display"/*i:Display*/, "is_display", GuiDiscover.MENU_ITEM_DISP_OPT);
+			mp.createAndAddIcon("Sounds & Alerts"/*i:SoundAlert*/, "is_sound", GuiDiscover.MENU_ITEM_SOUNDS_OPT);
 
-			mp.createAndAddIcon("Routing", "is_route", GuiDiscover.MENU_ITEM_ROUTING_OPT);
-			mp.createAndAddIcon("Recording Rules", "is_rec", GuiDiscover.MENU_ITEM_GPX_FILTER);
-			mp.createAndAddIcon("Export", "is_export", GuiDiscover.MENU_ITEM_GPX_DEVICE);
+			mp.createAndAddIcon("Routing"/*i:Routing*/, "is_route", GuiDiscover.MENU_ITEM_ROUTING_OPT);
+			mp.createAndAddIcon("Recording Rules"/*i:RecRules*/, "is_rec", GuiDiscover.MENU_ITEM_GPX_FILTER);
+			mp.createAndAddIcon("Export"/*i:Export*/, "is_export", GuiDiscover.MENU_ITEM_GPX_DEVICE);
 
-			mp.createAndAddIcon("Back", "is_back", IconActionPerformer.BACK_ACTIONID);
+			mp.createAndAddIcon("Back"/*i:Back*/, "is_back", IconActionPerformer.BACK_ACTIONID);
 			
 		// Advanced
-		mp = createAndAddMenuPage(" Advanced ", 3, 3);
-			mp.createAndAddIcon("Gui", "is_gui", GuiDiscover.MENU_ITEM_GUI_OPT);
-			mp.createAndAddIcon("Map Source", "is_map", GuiDiscover.MENU_ITEM_MAP_SRC);
-			mp.createAndAddIcon("Debug", "is_debug", GuiDiscover.MENU_ITEM_DEBUG_OPT);
+		mp = createAndAddMenuPage(" Advanced "/*i:Advanced*/, 3, 3);
+			mp.createAndAddIcon("Gui"/*i:Gui*/, "is_gui", GuiDiscover.MENU_ITEM_GUI_OPT);
+			mp.createAndAddIcon("Map Source"/*i:MapSource*/, "is_map", GuiDiscover.MENU_ITEM_MAP_SRC);
+			mp.createAndAddIcon("Debug"/*i:Debug*/, "is_debug", GuiDiscover.MENU_ITEM_DEBUG_OPT);
 
-			mp.createAndAddIcon("OpenCellID", "is_cellid", GuiDiscover.MENU_ITEM_OPENCELLID_OPT);
-			mp.createAndAddIcon("Keys", "is_keys", GuiDiscover.MENU_ITEM_KEYS_OPT);
-			mp.createAndAddIcon("Load Config", "is_load", GuiDiscover.MENU_ITEM_LOAD_CONFIG);
-			mp.createAndAddIcon("Save Config", "is_save", GuiDiscover.MENU_ITEM_SAVE_CONFIG);			
+			mp.createAndAddIcon("OpenCellID"/*i:OpenCellID*/, "is_cellid", GuiDiscover.MENU_ITEM_OPENCELLID_OPT);
+			mp.createAndAddIcon("Keys"/*i:Keys*/, "is_keys", GuiDiscover.MENU_ITEM_KEYS_OPT);
+			mp.createAndAddIcon("Load Config"/*i:LoadConfig*/, "is_load", GuiDiscover.MENU_ITEM_LOAD_CONFIG);
+			mp.createAndAddIcon("Save Config"/*i:SaveConfig*/, "is_save", GuiDiscover.MENU_ITEM_SAVE_CONFIG);
 	}
 
 }
