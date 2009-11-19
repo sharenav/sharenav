9d1845f57fa703ea021335c1b1528581b63f440f
sk750 <sk750@users.sourceforge.net>
2009-11-19 16:25:55 +0000

    Bugfix SingleTile: Wait for tile data until it is ready instead of timing out. This is a fix by james22

--- GpsMidGraph/de/ueller/gpsMid/mapData/SingleTile.java
+++ GpsMidGraph/de/ueller/gpsMid/mapData/SingleTile.java
@@ -112,7 +112,7 @@ public class SingleTile extends Tile implements QueueableTile {
 		}
 		
 		if (contain(pc)) {
-			if (!isDataReady()) {
+			while (!isDataReady()) {
 				if ((opt & Tile.OPT_WAIT_FOR_LOAD) == 0){
 					/**
 					 * We don't have the data yet. No need to wait, we 
