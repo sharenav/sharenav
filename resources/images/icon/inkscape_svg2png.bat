c7a442de14206bc8b17a5c682b95e45956d0d091
sk750 <sk750@users.sourceforge.net>
2009-11-20 19:55:11 +0000

    Icons: make the default icons included 32 pixels instead of 24 pixels wide for better quality. Also allow the .properties parameter useIcons=false|small|true|big  true is the default medium size, small is 24 pixels wide and big is 48 pixels wide

--- resources/images/icon/inkscape_svg2png.bat
+++ resources/images/icon/inkscape_svg2png.bat
@@ -11,35 +11,46 @@
 )
 
 cd main
-call :svg2png
+call :svg2png 24 small_
+call :svg2png 32
+call :svg2png 48 big_
 cd..
 
 cd rec
-call :svg2png
+call :svg2png 24 small_
+call :svg2png 32
+call :svg2png 48 big_
 cd..
 
 cd route
-call :svg2png
+call :svg2png 24 small_
+call :svg2png 32
+call :svg2png 48 big_
 cd..
 
 cd osm
-call :svg2png
+call :svg2png 24 small_
+call :svg2png 32
+call :svg2png 48 big_
 cd..
 
 cd setup
-call :svg2png
+call :svg2png 24 small_
+call :svg2png 32
+call :svg2png 48 big_
 cd..
 
 
 pause
 exit
 
+REM %1: edgeLen of icon  Parameter %2: prefix for png
 :svg2png
 REM Konvertieren der svg-Dateien im aktuellen Verzeichnis nach png
-for /R %%i in (*.svg) do start /wait %ProgramFiles%\inkscape\inkscape.exe -f "%%~fi" -e "%%~dpni.png" -D -w 24
+for /R %%i in (*.svg) do start /wait %ProgramFiles%\inkscape\inkscape.exe -f "%%~fi" -e "%%~dpi%2%%~ni.png" -D -w %1
 
 REM Komprimieren der png-Dateien in den Unterordner crushed
-..\pngcrush -d crushed -brute *.png
+..\pngcrush >nul -d crushed -brute %2*.png
 REM Ersetzen der png-Dateien durch die komprimierten aus dem Unterordner crushed
 copy crushed\*.png .
 REM Löschen des Unterordners crushed
