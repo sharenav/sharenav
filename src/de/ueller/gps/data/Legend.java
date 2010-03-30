54dc057f50793c20eb57b17c7858dff6bb58f2d5
james22 <james22@users.sourceforge.net>
2010-03-30 14:51:28 +0000

    update to new file version

--- src/de/ueller/gps/data/Legend.java
+++ src/de/ueller/gps/data/Legend.java
@@ -21,7 +21,7 @@ public class Legend {
 	 * Specifies the format of the map on disk we expect to see
 	 * This constant must be in sync with Osm2GpsMid
 	 */
-	public final static short MAP_FORMAT_VERSION = 54;
+	public final static short MAP_FORMAT_VERSION = 55;
 	
 	/** The waypoint format used in the RecordStore. See PositionMark.java. */
 	public final static short WAYPT_FORMAT_VERSION = 2;
