2bcf3479461afb11b3267aebd4a75bdb4783860c
mbaeurle <mbaeurle@users.sourceforge.net>
2010-07-03 16:10:21 +0000

    Corrected typo "Moving" instead of "Moveing", added copyright header, code formatting as done by Eclipse.

--- src/de/ueller/midlet/gps/data/ProjFactory.java
+++ src/de/ueller/midlet/gps/data/ProjFactory.java
@@ -1,35 +1,46 @@
+/*
+ * GpsMid - Copyright (c) 2008 Harald Mueller james22 at users dot sourceforge dot net 
+ * See COPYING
+ */
+
 package de.ueller.midlet.gps.data;
 
 public final class ProjFactory {
 
-	public static final byte NORTH_UP=0;
-	public static final byte MOVE_UP=1;
-	public static final byte MOVE_UP_ENH=2;
-	public static final byte EAGLE=3;
-	public static final String[] name={"North Up","Moveing", "Moveing enhanced", "Eagle"};
-	public static byte type=NORTH_UP;
-	
-	public static Projection getInstance(Node center, int upDir, float scale, int width, int height){
-		upDir=upDir%360;
-		switch (type){
-			case NORTH_UP:return new Proj2D(center,scale,width,height);
-			case MOVE_UP:return new Proj2DMoveUp(center,upDir,scale,width,height);
-			case MOVE_UP_ENH:return new Proj2DEnh(center,upDir,scale,width,height);
-			case EAGLE:return new Proj3D(center,upDir,scale,width,height);
+	public static final byte NORTH_UP = 0;
+	public static final byte MOVE_UP = 1;
+	public static final byte MOVE_UP_ENH = 2;
+	public static final byte EAGLE = 3;
+	public static final String[] name = { "North Up", "Moving",
+			"Moving enhanced", "Eagle" };
+	public static byte type = NORTH_UP;
+
+	public static Projection getInstance(Node center, int upDir, float scale,
+			int width, int height) {
+		upDir = upDir % 360;
+		switch (type) {
+		case NORTH_UP:
+			return new Proj2D(center, scale, width, height);
+		case MOVE_UP:
+			return new Proj2DMoveUp(center, upDir, scale, width, height);
+		case MOVE_UP_ENH:
+			return new Proj2DEnh(center, upDir, scale, width, height);
+		case EAGLE:
+			return new Proj3D(center, upDir, scale, width, height);
 		}
-		return new Proj2D(center,scale,width,height);
-	}
-	
-	public static void setProj(byte t){
-		type=t;
+		return new Proj2D(center, scale, width, height);
 	}
 
-	public static byte getProj(){
+	public static void setProj(byte t) {
+		type = t;
+	}
+
+	public static byte getProj() {
 		return type;
 	}
-	
-	public static String nextProj(){
-		type=(byte) ((type+1)%4);
+
+	public static String nextProj() {
+		type = (byte)((type + 1) % 4);
 		return name[type];
 	}
 
