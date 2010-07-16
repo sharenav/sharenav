53f914795083e7c37c9c7216fde309183f2c5e0d
jkpl <jkpj@users.sourceforge.net>
2010-07-16 08:52:21 +0000

    i18n for GuiDiscoverIconMenu

--- src/de/ueller/midlet/gps/GuiDiscoverIconMenu.java
+++ src/de/ueller/midlet/gps/GuiDiscoverIconMenu.java
@@ -21,30 +21,30 @@ public class GuiDiscoverIconMenu extends IconMenuWithPagesGUI {
 	
 		IconMenuPage mp;
 		// Basic
-		mp = createAndAddMenuPage(" Basic "/*i:Basic*/, 3, 3);
-			mp.createAndAddIcon("Location Receiver"/*i:LocReceiver*/, "is_loc", GuiDiscover.MENU_ITEM_LOCATION);
-			mp.createAndAddIcon("Display"/*i:Display*/, "is_display", GuiDiscover.MENU_ITEM_DISP_OPT);
-			mp.createAndAddIcon("Sounds & Alerts"/*i:SoundAlert*/, "is_sound", GuiDiscover.MENU_ITEM_SOUNDS_OPT);
+		mp = createAndAddMenuPage(Locale.get("guidiscovericonmenu.Basic")/* Basic */, 3, 3);
+			mp.createAndAddIcon(Locale.get("guidiscovericonmenu.LocReceiver")/*Location Receiver*/, "is_loc", GuiDiscover.MENU_ITEM_LOCATION);
+			mp.createAndAddIcon(Locale.get("guidiscovericonmenu.Display")/*Display*/, "is_display", GuiDiscover.MENU_ITEM_DISP_OPT);
+			mp.createAndAddIcon(Locale.get("guidiscovericonmenu.SoundAlert")/*Sounds "Sounds & Alerts" /* i:SoundAlert */ Alerts*/, "is_sound", GuiDiscover.MENU_ITEM_SOUNDS_OPT);
 
-			mp.createAndAddIcon("Routing"/*i:Routing*/, "is_route", GuiDiscover.MENU_ITEM_ROUTING_OPT);
-			mp.createAndAddIcon("Recording Rules"/*i:RecRules*/, "is_rec", GuiDiscover.MENU_ITEM_GPX_FILTER);
-			mp.createAndAddIcon("Export"/*i:Export*/, "is_export", GuiDiscover.MENU_ITEM_GPX_DEVICE);
+			mp.createAndAddIcon(Locale.get("guidiscovericonmenu.Routing")/*Routing*/, "is_route", GuiDiscover.MENU_ITEM_ROUTING_OPT);
+			mp.createAndAddIcon(Locale.get("guidiscovericonmenu.RecRules")/*Recording Rules*/, "is_rec", GuiDiscover.MENU_ITEM_GPX_FILTER);
+			mp.createAndAddIcon(Locale.get("guidiscovericonmenu.Export")/*Export*/, "is_export", GuiDiscover.MENU_ITEM_GPX_DEVICE);
 
-			mp.createAndAddIcon("Back"/*i:Back*/, "is_back", IconActionPerformer.BACK_ACTIONID);
+			mp.createAndAddIcon(Locale.get("guidiscovericonmenu.Back")/*Back*/, "is_back", IconActionPerformer.BACK_ACTIONID);
 			
 		// Advanced
-		mp = createAndAddMenuPage(" Advanced "/*i:Advanced*/, 3, 3);
-			mp.createAndAddIcon("Gui"/*i:Gui*/, "is_gui", GuiDiscover.MENU_ITEM_GUI_OPT);
-			mp.createAndAddIcon("Map Source"/*i:MapSource*/, "is_map", GuiDiscover.MENU_ITEM_MAP_SRC);
-			mp.createAndAddIcon("Debug"/*i:Debug*/, "is_debug", GuiDiscover.MENU_ITEM_DEBUG_OPT);
+		mp = createAndAddMenuPage(Locale.get("guidiscovericonmenu.Advanced")/* Advanced */, 3, 3);
+			mp.createAndAddIcon(Locale.get("guidiscovericonmenu.Gui")/*Gui*/, "is_gui", GuiDiscover.MENU_ITEM_GUI_OPT);
+			mp.createAndAddIcon(Locale.get("guidiscovericonmenu.MapSource")/*Map Source*/, "is_map", GuiDiscover.MENU_ITEM_MAP_SRC);
+			mp.createAndAddIcon(Locale.get("guidiscovericonmenu.Debug")/*Debug*/, "is_debug", GuiDiscover.MENU_ITEM_DEBUG_OPT);
 
 			//#if polish.api.osm-editing
-			mp.createAndAddIcon("OSM Account"/*i:OSMAccount*/, "is_osmacc", GuiDiscover.MENU_ITEM_OSM_OPT);
+			mp.createAndAddIcon(Locale.get("guidiscovericonmenu.OSMAccount")/*OSM Account*/, "is_osmacc", GuiDiscover.MENU_ITEM_OSM_OPT);
 			//#endif
-			mp.createAndAddIcon("OpenCellID"/*i:OpenCellID*/, "is_cellid", GuiDiscover.MENU_ITEM_OPENCELLID_OPT);
-			mp.createAndAddIcon("Keys"/*i:Keys*/, "is_keys", GuiDiscover.MENU_ITEM_KEYS_OPT);
-			mp.createAndAddIcon("Load Config"/*i:LoadConfig*/, "is_load", GuiDiscover.MENU_ITEM_LOAD_CONFIG);
-			mp.createAndAddIcon("Save Config"/*i:SaveConfig*/, "is_save", GuiDiscover.MENU_ITEM_SAVE_CONFIG);
+			mp.createAndAddIcon(Locale.get("guidiscovericonmenu.OpenCellID")/*OpenCellID*/, "is_cellid", GuiDiscover.MENU_ITEM_OPENCELLID_OPT);
+			mp.createAndAddIcon(Locale.get("guidiscovericonmenu.Keys")/*Keys*/, "is_keys", GuiDiscover.MENU_ITEM_KEYS_OPT);
+			mp.createAndAddIcon(Locale.get("guidiscovericonmenu.LoadConfig")/*Load Config*/, "is_load", GuiDiscover.MENU_ITEM_LOAD_CONFIG);
+			mp.createAndAddIcon(Locale.get("guidiscovericonmenu.SaveConfig")/*Save Config*/, "is_save", GuiDiscover.MENU_ITEM_SAVE_CONFIG);
 
 			setActiveTabAndCursor(rememberedTabNr, rememberedEleId);
 	}
