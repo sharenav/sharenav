7fe30480f8ec09501f3aabf26ffee7b84a5baa4b
jkpl <jkpj@users.sourceforge.net>
2010-09-03 18:00:28 +0000

    borders for current route path, thanks karel

--- GpsMidGraph/de/ueller/midlet/gps/data/Way.java
+++ GpsMidGraph/de/ueller/midlet/gps/data/Way.java
@@ -1597,7 +1597,7 @@ public class Way extends Entity {
 						setBorderColor(pc, wayDesc,(hl[i] >= 0), 
 							       (isCurrentRoutePath(pc, i) || dividedHighlight), 
 							       (highlight == HIGHLIGHT_DEST));
-						if (! Configuration.getCfgBitState(Configuration.CFGBIT_NOSTREETBORDERS) ) {
+						if (! Configuration.getCfgBitState(Configuration.CFGBIT_NOSTREETBORDERS) || isCurrentRoutePath(pc, i)) {
 						    pc.g.drawLine(l2b.x, l2b.y, l2e.x, l2e.y);
 						    pc.g.drawLine(l1b.x, l1b.y, l1e.x, l1e.y);
 						}
@@ -1635,7 +1635,7 @@ public class Way extends Entity {
 							setBorderColor(pc, wayDesc, (hl[i] >= 0), 
 								       (isCurrentRoutePath(pc, i) || dividedHighlight), 
 								       (highlight == HIGHLIGHT_DEST));
-							if (! Configuration.getCfgBitState(Configuration.CFGBIT_NOSTREETBORDERS)) {
+							if (! Configuration.getCfgBitState(Configuration.CFGBIT_NOSTREETBORDERS) || isCurrentRoutePath(pc, i)) {
 							    if (highlight == HIGHLIGHT_NONE) {
 								//paint the inner turn border to the intersection point between old and current waysegment 
 								pc.g.drawLine(intersecP.x, intersecP.y, l2e.x, l2e.y);
@@ -1672,7 +1672,7 @@ public class Way extends Entity {
 							setBorderColor(pc, wayDesc, (hl[i] >= 0), 
 									(isCurrentRoutePath(pc, i) || dividedHighlight), 
 									(highlight == HIGHLIGHT_DEST));
-							if (! Configuration.getCfgBitState(Configuration.CFGBIT_NOSTREETBORDERS)) {
+							if (! Configuration.getCfgBitState(Configuration.CFGBIT_NOSTREETBORDERS) || isCurrentRoutePath(pc, i)) {
 								if (highlight == HIGHLIGHT_NONE) {
 									//see comments above
 									pc.g.drawLine(intersecP.x, intersecP.y, l1e.x, l1e.y);
@@ -1703,7 +1703,7 @@ public class Way extends Entity {
 							setBorderColor(pc, wayDesc, (hl[i] >= 0), 
 									(isCurrentRoutePath(pc, i) || dividedHighlight), 
 									(highlight == HIGHLIGHT_DEST));
-							if (! Configuration.getCfgBitState(Configuration.CFGBIT_NOSTREETBORDERS)) {
+							if (! Configuration.getCfgBitState(Configuration.CFGBIT_NOSTREETBORDERS) || isCurrentRoutePath(pc, i)) {
 								pc.g.drawLine(l2b.x, l2b.y, l2e.x, l2e.y);
 								// paint the full outer turn border
 								pc.g.drawLine(l1b.x, l1b.y, l1e.x, l1e.y);
