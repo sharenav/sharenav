8512e05f16a487c8a0854f33e77d5ac79c0a00d6
Markus Bäurle <mbaeurle@users.sourceforge.net>
2010-11-02 01:01:35 +0100

    Use a thread when saving a waypoint. This seems to reduce the number of freezes during this operation on my phone a lot.

--- src/de/ueller/midlet/gps/GuiWaypointSave.java
+++ src/de/ueller/midlet/gps/GuiWaypointSave.java
@@ -1,8 +1,9 @@
-package de.ueller.midlet.gps;
 /*
  * GpsMid - Copyright (c) 2008 Kai Krueger apmonkey at users dot sourceforge dot net 
- * See Copying
+ * See COPYING
  */
+
+package de.ueller.midlet.gps;
 
 import de.enough.polish.util.Locale;
 
@@ -19,13 +20,15 @@ import javax.microedition.lcdui.ChoiceGroup;
 
 
 public class GuiWaypointSave extends Form implements CommandListener {
-	private TextField fldName,fldEle;
+	private TextField fldName;
+	private TextField fldEle;
 	private ChoiceGroup cg;
 	private static final Command saveCmd = new Command(Locale.get("guiwaypointsave.Save")/*Save*/, Command.OK, 1);
 	private static final Command backCmd = new Command(Locale.get("generic.Back")/*Back*/, Command.BACK, 2);
 	
-	private Trace parent;
-	private String name,ele;
+	private final Trace parent;
+	private String name;
+	private String ele;
 	private PositionMark waypt;
 	
 	protected static final Logger logger = Logger.getInstance(GuiWaypointSave.class,Logger.TRACE);
@@ -41,11 +44,13 @@ public class GuiWaypointSave extends Form implements CommandListener {
 	}
 
 	private void jbInit() throws Exception {
-		fldName = new TextField(Locale.get("guiwaypointsave.Name")/*Name:*/, "", Configuration.MAX_WAYPOINTNAME_LENGTH, TextField.ANY);
-		fldEle = new TextField(Locale.get("guiwaypointsave.AltitudeInM")/*Altitude (in m):*/, "", 5, TextField.NUMERIC);
+		fldName = new TextField(Locale.get("guiwaypointsave.Name")/*Name:*/, "", 
+				Configuration.MAX_WAYPOINTNAME_LENGTH, TextField.ANY);
+		fldEle = new TextField(Locale.get("guiwaypointsave.AltitudeInM")/*Altitude (in m):*/, 
+				"", 5, TextField.NUMERIC);
 		cg = new ChoiceGroup(Locale.get("guiwaypointsave.Settings")/*Settings*/, Choice.MULTIPLE);
-		cg.append(Locale.get("guiwaypointsave.Favorite")/*Favorite*/,null);
-		cg.append(Locale.get("guiwaypointsave.RecenterToGPS")/*Recenter to GPS after saving*/,null);
+		cg.append(Locale.get("guiwaypointsave.Favorite")/*Favorite*/, null);
+		cg.append(Locale.get("guiwaypointsave.RecenterToGPS")/*Recenter to GPS after saving*/, null);
 		
 		// Set up this Displayable to listen to command events
 		setCommandListener(this);
@@ -58,9 +63,9 @@ public class GuiWaypointSave extends Form implements CommandListener {
 		this.append(cg);
 	}
 	
-	public void setData(PositionMark pos)
+	public void setData(PositionMark posMark)
 	{
-		this.waypt = pos;
+		this.waypt = posMark;
 		this.name = "";
 		fldName.setString(name);
 		
@@ -70,7 +75,6 @@ public class GuiWaypointSave extends Form implements CommandListener {
 		} else {
 			fldEle.setString("");			
 		}
-		
 	}
 
 	public void commandAction(Command cmd, Displayable displayable) {
@@ -83,22 +87,35 @@ public class GuiWaypointSave extends Form implements CommandListener {
 			ele  = fldEle.getString();
 			logger.info("Saving waypoint with name: " + name + " ele: " + ele);
 			waypt.displayName = name;
-			
-			try {
-				//use the value from textfield, if not "" 
-				waypt.ele = Integer.parseInt(ele);
-			} catch (NumberFormatException e) {
-				waypt.ele = PositionMark.INVALID_ELEVATION;
-			}
-		            
-			parent.gpx.addWayPt(waypt);
+			Thread adder = new Thread(new Runnable() {
+				public void run()
+				{
+					try {
+						// Use the elevation from textfield, if not "" 
+						waypt.ele = Integer.parseInt(ele);
+					} catch (NumberFormatException e) {
+						waypt.ele = PositionMark.INVALID_ELEVATION;
+					}
+					
+					parent.gpx.addWayPt(waypt);
 
-			// Recenter GPS after saving a Waypoint if this option is selected
-			if (cg.isSelected(1)) {
-				parent.gpsRecenter = true;
-				parent.newDataReady();
-			}
-			parent.show();				
+					// Wait a bit before displaying the map again. Hopefully
+					// this avoids the sporadic freezing after saving a waypoint.
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException ie) {
+					}
+
+					// Recenter GPS after saving a Waypoint if this option is selected
+					if (cg.isSelected(1)) {
+						parent.gpsRecenter = true;
+						parent.newDataReady();
+					}
+					parent.show();				
+				}
+			} );
+			adder.setPriority(Thread.MAX_PRIORITY);
+			adder.start();
 			return;
 		}
 		else if (cmd == backCmd) {
@@ -108,6 +125,6 @@ public class GuiWaypointSave extends Form implements CommandListener {
 	}
 	
 	public void show() {
-			GpsMid.getInstance().show(this);
+		GpsMid.getInstance().show(this);
 	}
 }
