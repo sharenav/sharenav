9a22fa8479164277157b4d2e4ab041b6a487d92b
jkpj <jkpj@users.sourceforge.net>
2010-11-12 01:58:28 +0200

    restore correct file accidentally emptied

--- src/de/ueller/midlet/gps/importexport/Jsr172GpxParser.java
+++ src/de/ueller/midlet/gps/importexport/Jsr172GpxParser.java
@@ -1,7 +1,5 @@
 /*
- * GpsMid - Copyright (c) 2007 Harald Mueller james22 at users dot sourceforge dot net
- *        - Copyright (c) 2008 Kai Krueger apmonkey at users dot sourceforge dot net
- *        - Copyright (c) 2008 sk750 at users dot sourceforge dot net
+ * GpsMid - Copyright (c) 2008, 2009 
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -12,330 +10,86 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
- * See Copying
  */
-package de.ueller.gps.location;
+//#if polish.api.webservice
+package de.ueller.midlet.gps.importexport;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Vector;
+import java.io.InputStream;
+import java.util.Hashtable;
 
-//#if polish.api.locationapi
-import javax.microedition.location.Coordinates;
-import javax.microedition.location.Criteria;
-import javax.microedition.location.Location;
-import javax.microedition.location.LocationException;
-import javax.microedition.location.LocationListener;
-import javax.microedition.location.LocationProvider;
-//#endif
-import de.ueller.gps.data.Position;
-import de.ueller.gps.nmea.NmeaMessage;
-import de.ueller.gps.tools.StringTokenizer;
-import de.ueller.midlet.gps.LocationMsgProducer;
-import de.ueller.midlet.gps.LocationMsgReceiver;
-import de.ueller.midlet.gps.LocationMsgReceiverList;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
 import de.ueller.midlet.gps.Logger;
-//FIXME make a proper interface for passing fix age information instead of accessing trace variable
-import de.ueller.midlet.gps.Trace;
-import de.ueller.gps.data.Configuration;
 
-import de.enough.polish.util.Locale;
-
-/**
- * This class implements a location producer which uses the JSR-179 API
- * to get the device's current position.
- * This API is supported by most phones with an internal GPS receiver.
- */
-public class JSR179Input 
-		//#if polish.api.locationapi
-		implements LocationListener, LocationMsgProducer
-		//#endif
-{
-	private final static Logger logger = Logger.getInstance(JSR179Input.class,
-			Logger.TRACE);
-
-	//#if polish.api.locationapi
-	private LocationProvider locationProvider = null;
-	private LocationMsgReceiverList receiverList;
-	private NmeaMessage smsg;
-	Position pos = new Position(0f, 0f, 0f, 0f, 0f, 0, System.currentTimeMillis());
-
-	private Trace tr = Trace.getInstance();
-
-	private OutputStream rawDataLogger;
-
-	
-	public JSR179Input() {
-		this.receiverList = new LocationMsgReceiverList();
-	}
-
-
-	public boolean init(LocationMsgReceiver receiver) {
-		logger.info("start JSR179 LocationProvider");
-		this.receiverList.addReceiver(receiver);
-		// We may be able to get some additional information such as the number
-		// of satellites form the NMEA string
-		smsg = new NmeaMessage(receiverList);
-		createLocationProvider();
-		
-		return true;
-	}
-
-	public boolean activate(LocationMsgReceiver receiver) {
-		// FIXME move activation code (code to enable continuos location feed) here
-		return true;
-	}
-	public boolean deactivate(LocationMsgReceiver receiver) {
-		return true;
-	}
+public class Jsr172GpxParser extends DefaultHandler implements GpxParser {
 
 	/**
-	 * Initializes LocationProvider uses default criteria
-	 * 
-	 * @throws Exception
+	 * This is a wrapper for the JSR172 parser. We need this wrapper so that we
+	 * can use the same code for both the QDGpx parser and the JSR-172 parser.
+	 * Although they are close to identical, they aren't completely. Hence the
+	 * need for this wrapper. We also need the wrapper to guard against missing
+	 * JSRs by calling this through the Class.forName()
 	 */
-	void createLocationProvider() {
-		logger.trace("enter createLocationProvider()");
-		if (locationProvider == null) {
-			// try out different locationprovider criteria combinations, the
-			// ones with maximum features first
-			for (int i = 0; i <= 3; i++) {
-				try {
-					Criteria criteria = new Criteria();
-					switch (i) {
-					case 0:
-						criteria.setAltitudeRequired(true);
-						criteria.setSpeedAndCourseRequired(true);
-						break;
-					case 1:
-						criteria.setSpeedAndCourseRequired(true);
-						break;
-					case 2:
-						criteria.setAltitudeRequired(true);
-					}
-					locationProvider = LocationProvider.getInstance(criteria);
-					if (locationProvider != null) {
-						logger.info("Chosen location provider:" + locationProvider);
-						break; // we are using this criteria
-					}
-				} catch (LocationException le) {
-					logger.info("LocationProvider criteria not fitting: " + i);
-					locationProvider = null;
-				} catch (Exception e) {
-					logger.exception(Locale.get("jsr179input.unexpectedExceptioninLocProv")/*unexpected exception while probing LocationProvider criteria.*/,e);
-				}
-			}
-			if (locationProvider != null) {
-				try {
-					locationProvider.setLocationListener(this, 1, -1, -1);
-				} catch (Exception e) {
-					receiverList.receiveSolution("SecEx"/*SecEx*/);
-					locationProvider = null;
-				}
-				if (locationProvider != null)  {
-					updateSolution(locationProvider.getState());
-				}
-			} else {
-				receiverList.locationDecoderEnd(Locale.get("jsr179input.NoJSR179Provider")/*no JSR179 Provider*/);
-				//#debug info
-				logger.info("Cannot create LocationProvider for criteria.");
-			}
+
+	private final static Logger logger = Logger.getInstance(
+			Jsr172GpxParser.class, Logger.DEBUG);
+
+	private XmlParserContentHandler contentH;
+
+	public void startElement(String namespaceURI, String localName,
+			String qName, Attributes atts) {
+		Hashtable attTable = new Hashtable();
+		for (int i = 0; i < atts.getLength(); i++) {
+			attTable.put(atts.getQName(i), atts.getValue(i));
 		}
-		//#debug
-		logger.trace("exit createLocationProvider()");
+		contentH.startElement(namespaceURI, localName, qName, attTable);
 	}
 
-	public void locationUpdated(LocationProvider provider, Location location) {
-		//#debug info
-		logger.info("updateLocation: " + location);
-		if (location == null) {
-			return;
+	public void endElement(String namespaceURI, String localName, String qName) {
+		contentH.endElement(namespaceURI, localName, qName);
+	}
+
+	public void startDocument() {
+		contentH.startDocument();
+	}
+
+	public void endDocument() {
+		contentH.endDocument();
+	}
+
+	public void characters(char[] ch, int start, int length) {
+		contentH.characters(ch, start, length);
+	}
+
+	public boolean parse(InputStream in, XmlParserContentHandler contentH) {
+		if (contentH == null) {
+			logger.error("Can't parse XML without a content handler");
+			return false;
 		}
-		//#debug debug
-		logger.debug("received Location: " + location.getLocationMethod());
-		
-		String nmeaString = location
-				.getExtraInfo("application/X-jsr179-location-nmea");
-		//nmeaString =
-		//	 "$GPGSA,A,3,32,23,31,11,20,,,,,,,,2.8,2.6,1.0*3D$GPGSV,3,1,10,20,71,071,38,23,60,168,41,17,42,251,25,11,36,148,37*73$GPGSV,3,2,10,04,29,300,16,13,26,197,,31,21,054,47,32,10,074,38*70$GPGSV,3,3,10,12,04,339,17,05,01,353,15*75";
-		// ;
-		//#debug info
-		logger.info("Using extra NMEA info in JSR179: " + nmeaString);
-		
-		if (nmeaString != null) {
-			if (rawDataLogger != null) {
-				try {
-					rawDataLogger.write(nmeaString.getBytes());
-					rawDataLogger.flush();
-				} catch (IOException ioe) {
-					logger.exception(Locale.get("jsr179input.CouldNotWriteGPSLog")/*Could not write raw GPS log*/, ioe);
-				}
-			}
-			Vector messages = StringTokenizer.getVector(nmeaString, "$");
-			if (messages != null) {
-				for (int i = 0; i < messages.size(); i++) {
-					String nmeaMessage = (String) messages.elementAt(i);
-					if (nmeaMessage == null) {
-						continue;
-					}
-					if (nmeaMessage.startsWith("$")) {
-						// Cut off $GP from the start
-						nmeaMessage = nmeaMessage.substring(3);
-					} else if (nmeaMessage.startsWith("GP")) {
-						// Cut off GP from the start
-						nmeaMessage = nmeaMessage.substring(2);
-					}
-					int delimiterIdx = nmeaMessage.indexOf("*");
-					if (delimiterIdx > 0) {
-						// remove the checksum
-						nmeaMessage = nmeaMessage.substring(0, delimiterIdx);
-					}
-					delimiterIdx = nmeaMessage.indexOf(" ");
-					if (delimiterIdx > 0) {
-						// remove trailing whitespace because some mobiles like HTC Touch Diamond 2 with JavaFX
-						// receive NMEA sentences terminated by a space instead of a star followed by the checksum
-						nmeaMessage = nmeaMessage.substring(0, delimiterIdx);
-					}
-					//#debug info
-					logger.info("Decoding: " + nmeaMessage);
-					if ((nmeaMessage != null) && (nmeaMessage.length() > 5))
-						smsg.decodeMessage(nmeaMessage, false);
-				}
-			}
+		this.contentH = contentH;
+		try {
+			logger.debug("Starting to parse XML document with JSR-172 parser");
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+			// Parse the input
+			SAXParser saxParser = factory.newSAXParser();
+			saxParser.parse(in, this);
+			return true;
+		} catch (SAXException e) {
+			logger.exception("Sax Parser Error", e);
+		} catch (IOException e) {
+			logger.exception("A Problem reading the XML file occured", e);
+		} catch (ParserConfigurationException e) {
+			logger.exception("Your parser was misconfigured", e);
 		}
+		return false;
 
-		if (location.isValid()) {
-			/* e.g. SE C702 only receives from getExtraInfo() $GPGSV sentences,
-			 * therefore use On as solution when it's still set to NoFix though the location is valid
-			 */
-			if (receiverList.getCurrentSolution().equals(Locale.get("jsr179input.NoFix")/*NoFix*/)) {
-				receiverList.receiveSolution(Locale.get("generic.On")/*On*/);				
-			}
-			Coordinates coordinates = location.getQualifiedCoordinates();
-			pos.latitude = (float) coordinates.getLatitude();
-			pos.longitude = (float) coordinates.getLongitude();
-			pos.altitude = (float) coordinates.getAltitude();
-			pos.course = location.getCourse();
-			pos.speed = location.getSpeed();
-			pos.timeMillis = location.getTimestamp();
-			receiverList.receivePosition(pos);
-		} else {
-			if (receiverList != null) {
-				receiverList.receiveSolution(Locale.get("jsr179input.NoFix")/*NoFix*/);
-			}
-		}
-		// logger.trace("exit locationUpdated(provider,location)");
 	}
-
-	public void providerStateChanged(LocationProvider lprov, int state) {
-		//#debug info
-		logger.info("providerStateChanged(" + lprov + "," + state + ")");
-		updateSolution(state);
-	}
-
-	public void close() {
-		//#debug
-		logger.trace("enter close()");
-		// if (locationProvider != null){
-		// locationProvider.setLocationListener(null, -1, -1, -1);
-		// }
-		if (locationProvider != null) {
-/* locationProvider.setLocationListener(null, 0, 0, 0) tends to freeze Samsung S8000 frequently when stopping GPS
- * was replaced by locationProvider.reset() until 2010-10-21.
- * So if other mobile devices have issues with locationProvider.reset(),
- * please document this here
- * - Aparently Gps is left on, on at least Nokia S40 phones and SE G705, without 
- * locationProvider.setLocationListener(null, 0, 0, 0) - hopefully both reset()
- * and setLocationListener(null... will work on all phones.
- */  
-			locationProvider.reset();
-			locationProvider.setLocationListener(null, 0, 0, 0);
-		}
-		locationProvider = null;
-		receiverList.locationDecoderEnd();
-		//#debug
-		logger.trace("exit close()");
-	}
-
-	private void updateSolution(int state) {
-		logger.info("Update Solution");
-		// if we have a current cell id fix from startup cellid location,
-		// don't overwrite it with a stale GPS location
-		if (Configuration.getCfgBitSavedState(Configuration.CFGBIT_CELLID_STARTUP) && tr.solution.equals(Locale.get("secellid.Cell"))) {
-			// do nothing
-		} else {
-			// pass last known GPS location
-			//FIXME make a proper interface for passing fix age information instead of accessing trace variable directly
-			tr.gpsRecenterInvalid = true;
-			tr.gpsRecenterStale = true;
-			locationUpdated(locationProvider, LocationProvider.getLastKnownLocation());
-		}
-		if (state == LocationProvider.AVAILABLE) {
-			if (receiverList != null) {
-				receiverList.receiveSolution(Locale.get("jsr179input.NoFix")/*NoFix*/);
-			}
-		}
-		//#if polish.android
-		// FIXME when j2mepolish is fixed; 2010-06 j2mepolish gives OUT_OF_SERVICE
-		// even when waiting for a fix, so we don't want to switch GPS off here
-		//#else
-		if (state == LocationProvider.OUT_OF_SERVICE) {
-			if (receiverList != null) {
-				receiverList.receiveSolution(Locale.get("generic.Off")/*Off*/);
-				receiverList.receiveMessage(Locale.get("jsr179input.ProviderStopped")/*provider stopped*/);
-			}
-		}
-		//#endif
-		//#if polish.android
-		if (state == LocationProvider.TEMPORARILY_UNAVAILABLE || state == LocationProvider.OUT_OF_SERVICE) {
-		//#else
-		if (state == LocationProvider.TEMPORARILY_UNAVAILABLE) {
-		//#endif
-			/**
-			 * Even though the receiver is temporarily un-available,
-			 * we still need to receive updates periodically, as some
-			 * implementations will otherwise not reacquire the GPS signal.
-			 * So setting setLocationListener to 0 interval, which should have given
-			 * you all the status changes, does not work.
-			 */
-			if (receiverList != null) {
-				receiverList.receiveSolution(Locale.get("jsr179input.NoFix")/*NoFix*/);
-			}
-		}
-	}
-
-	public void triggerPositionUpdate() {
-	}
-
-	public void triggerLastKnownPositionUpdate() {
-		//FIXME make a proper interface for passing fix age information instead of accessing trace variable directly
-		tr.gpsRecenterInvalid = true;
-		tr.gpsRecenterStale = true;
-		locationUpdated(locationProvider, LocationProvider.getLastKnownLocation());
-	}
-
-	public void disableRawLogging() {
-		if (rawDataLogger != null) {
-			try {
-				rawDataLogger.close();
-			} catch (IOException e) {
-				logger.exception(Locale.get("jsr179input.CouldntCloseRawGpsLogger")/*Couldnt close raw gps logger*/, e);
-			}
-			rawDataLogger = null;
-		}
-	}
-
-	public void enableRawLogging(OutputStream os) {
-		rawDataLogger = os;
-	}
-
-	public void addLocationMsgReceiver(LocationMsgReceiver receiver) {
-		receiverList.addReceiver(receiver);
-	}
-
-	public boolean removeLocationMsgReceiver(LocationMsgReceiver receiver) {
-		return receiverList.removeReceiver(receiver);
-	}
-	//#endif
 }
+//#endif
