57ad1445f5553cec8785ea531c9d33868f4d8605
Markus Bäurle <mbaeurle@users.sourceforge.net>
2010-12-20 02:54:45 +0100

    Build error in MyMathTest fixed. This is not the same commit as in my gitorious repository, I reapplied the changes to a fresh repository.

--- Osm2GpsMid/test/de/ueller/osmToGpsMid/MyMathTest.java
+++ Osm2GpsMid/test/de/ueller/osmToGpsMid/MyMathTest.java
@@ -27,6 +27,7 @@ public class MyMathTest extends TestCase {
 	/* (non-Javadoc)
 	 * @see junit.framework.TestCase#setUp()
 	 */
+	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 	}
@@ -34,9 +35,11 @@ public class MyMathTest extends TestCase {
 	/* (non-Javadoc)
 	 * @see junit.framework.TestCase#tearDown()
 	 */
+	@Override
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
+
 //	public void testSpherical_distance(){
 //		float lat=(float)Math.toRadians(45.0);
 //		float c;
@@ -52,21 +55,22 @@ public class MyMathTest extends TestCase {
 	 * Test method for {@link de.ueller.osmToGpsMid.MyMath#dist(de.ueller.osmToGpsMid.model.Node, de.ueller.osmToGpsMid.model.Node)}.
 	 */
 	public void testDist() {
-		Node nordpool=new Node(90f,0f,0);
-		Node aequador0=new Node(0f,0f,0);
-		Node gsteinach=new Node(49.3519420165977f,11.224289273197f,0);
-		Node ezelsdorf=new Node(49.3278456097341f,11.3470017176587f,0);
-		Node nuernberg=new Node(49.4473022343456f,11.0877756401478f,0);
-		Node hamburg=new Node(53.5500018787512f,10.0000116163133f,0);
-		Node wuerzburg=new Node(49.7909141367451f,9.97069016105521f,0);
-		System.out.println("Gsteinach - Ezelsdorf " + (MyMath.dist(gsteinach, ezelsdorf)/10000f) + " km");
-		System.out.println("Nürnberg - Ezelsdorf " + (MyMath.dist(nuernberg, ezelsdorf)/10000f) + " km");
-		System.out.println("Nürnberg - Hamburg " + (MyMath.dist(nuernberg, hamburg)/10000f) + " km");
-		System.out.println("Nürnberg - Würzburg " + (MyMath.dist(nuernberg, wuerzburg)/10000f) + " km");
-		System.out.println("Nordpool - Äquador " + (MyMath.dist(nordpool, aequador0)/10000f) + " km");
+		Node northpole = new Node(90f, 0f, 0);
+		Node equator0 = new Node(0f, 0f, 0);
+		Node gsteinach = new Node(49.3519420165977f, 11.224289273197f,0);
+		Node ezelsdorf = new Node(49.3278456097341f, 11.3470017176587f,0);
+		Node nuernberg = new Node(49.4473022343456f, 11.0877756401478f,0);
+		Node hamburg = new Node(53.5500018787512f, 10.0000116163133f,0);
+		Node wuerzburg = new Node(49.7909141367451f, 9.97069016105521f,0);
+		System.out.println("Gsteinach - Ezelsdorf " + (MyMath.dist(gsteinach, ezelsdorf) / 10000f) + " km");
+		System.out.println("Nürnberg - Ezelsdorf " + (MyMath.dist(nuernberg, ezelsdorf) / 10000f) + " km");
+		System.out.println("Nürnberg - Hamburg " + (MyMath.dist(nuernberg, hamburg) / 10000f) + " km");
+		System.out.println("Nürnberg - Würzburg " + (MyMath.dist(nuernberg, wuerzburg) / 10000f) + " km");
+		System.out.println("North pole - Equator0 " + (MyMath.dist(northpole, equator0) / 10000f) + " km");
 	}
-	public void testInversBearing(){
-		System.out.println("invers of 120 " + (MyMath.inversBearing((byte) 60))*2);
+
+	public void testInverseBearing(){
+		System.out.println("Inverse of 120 " + (MyMath.inverseBearing((byte) 60))*2);
 	}
 
 }
