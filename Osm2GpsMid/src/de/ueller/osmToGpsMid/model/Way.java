de0827fdfc14b34a725159aae9a96197ca93784c
jkpj <jkpj@users.sourceforge.net>
2011-05-02 12:51:13 +0300

    don't duplicate namekey for namefallback * (ways)

--- Osm2GpsMid/src/de/ueller/osmToGpsMid/model/Way.java
+++ Osm2GpsMid/src/de/ueller/osmToGpsMid/model/Way.java
@@ -293,7 +293,7 @@ public class Way extends Entity implements Comparable<Way> {
 				} else {
 					nameFallback = getAttribute(desc.nameFallbackKey);
 				}
-				if (name != null && nameFallback != null) {
+				if (name != null && nameFallback != null && (!desc.nameFallbackKey.equals("*") || !desc.key.equals(desc.nameKey))) {
 					name += " (" + nameFallback + ")";
 				} else if ((name == null) && (nameFallback != null)) {
 					name = nameFallback;
