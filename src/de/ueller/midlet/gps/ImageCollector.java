e335a49999597dc1ed0437dcda8994444dbdb3b2
jkpj <jkpj@users.sourceforge.net>
2011-06-12 16:04:07 +0300

    restore old wait behaviour for imagecollector

--- src/de/ueller/midlet/gps/ImageCollector.java
+++ src/de/ueller/midlet/gps/ImageCollector.java
@@ -121,7 +121,18 @@ public class ImageCollector implements Runnable {
 				if (!needRedraw || suspended) {
 					synchronized (this) {
 						try {
-							wait();
+							/* FIXME: We still have some situations where redraw is not done automatically immediately,
+							 * e.g. on Nokia 5800 after returning from another Displayable
+							 * Therefore reduce the timeout for redrawing anyway from 30 seconds to 1 seconds 
+							 * if the last user interaction happened less than 1.5 secs before 
+
+							if (Trace.getDurationSinceLastUserActionTime() > 1500 ) {
+								wait(30000);
+							} else {
+								wait(1000);
+							}
+							*/
+							wait(30000);
 						} catch (InterruptedException e) {
 							continue; // Recheck condition of the loop
 						}
@@ -149,7 +160,7 @@ public class ImageCollector implements Runnable {
 					
 					// System.out.println("img not ready");
 					try {
-						Thread.sleep(100);
+						wait(100);
 					} catch (InterruptedException e) {}
 					
 				}
