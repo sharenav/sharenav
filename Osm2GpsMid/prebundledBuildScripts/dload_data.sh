af73e4e116969304fdcd2bbc21bb8ece169862aa
Markus Bäurle <mbaeurle@users.sourceforge.net>
2012-01-03 00:31:24 +0100

    dload_data.sh: Downloads also grouped by continent but not really sorted (not sure whether to sort by name or URL) - no other changes.

--- Osm2GpsMid/prebundledBuildScripts/dload_data.sh
+++ Osm2GpsMid/prebundledBuildScripts/dload_data.sh
@@ -5,6 +5,73 @@
 
 wget http://myapp.fr/cellsIdData/cells.txt.gz -O /home/apmon/Osm2GpsMid/cells.txt.gz
 
+# Africa
+wget http://download.geofabrik.de/osm/africa/canary_islands.osm.pbf -O /home/apmon/planet_dumps/canary.osm.pbf
+wget http://download.geofabrik.de/osm/africa/libya.osm.pbf -O /home/apmon/planet_dumps/libya.osm.pbf
+wget http://download.geofabrik.de/osm/africa/south_africa_and_lesotho.osm.pbf -O /home/apmon/planet_dumps/south_africa.osm.pbf
+wget http://downloads.cloudmade.com/africa/eastern_africa/zimbabwe/zimbabwe.osm.bz2 -O /home/apmon/planet_dumps/zimbabwe.osm.bz2
+wget http://downloads.cloudmade.com/africa/eastern_africa/ethiopia/ethiopia.osm.bz2 -O /home/apmon/planet_dumps/ethiopia.osm.bz2
+wget http://downloads.cloudmade.com/africa/western_africa/nigeria/nigeria.osm.bz2 -O /home/apmon/planet_dumps/nigeria.osm.bz2
+wget http://downloads.cloudmade.com/africa/northern_africa/egypt/egypt.osm.bz2 -O /home/apmon/planet_dumps/egypt.osm.bz2
+wget http://downloads.cloudmade.com/africa/northern_africa/morocco/morocco.osm.bz2 -O /home/apmon/planet_dumps/morocco.osm.bz2
+wget http://downloads.cloudmade.com/africa/northern_africa/tunisia/tunisia.osm.bz2 -O /home/apmon/planet_dumps/tunesia.osm.bz2
+wget http://downloads.cloudmade.com/africa/middle_africa/democratic_republic_of_the_congo/democratic_republic_of_the_congo.osm.bz2 -O /home/apmon/planet_dumps/congo.osm.bz2
+wget http://downloads.cloudmade.com/africa/eastern_africa/kenya/kenya.osm.bz2 -O /home/apmon/planet_dumps/kenya.osm.bz2
+wget http://downloads.cloudmade.com/africa/eastern_africa/seychelles/seychelles.osm.bz2 -O /home/apmon/planet_dumps/seychelles.osm.bz2
+wget http://downloads.cloudmade.com/africa/southern_africa/namibia/namibia.osm.bz2 -O /home/apmon/planet_dumps/namibia.osm.bz2
+
+# Americas
+wget http://download.geofabrik.de/osm/central-america/haiti-and-domrep.osm.pbf -O /home/apmon/planet_dumps/haiti.osm.pbf
+wget http://download.geofabrik.de/osm/south-america/brazil.osm.pbf -O /home/apmon/planet_dumps/brazil.osm.pbf
+wget http://download.geofabrik.de/osm/south-america/colombia.osm.pbf -O /home/apmon/planet_dumps/colombia.osm.pbf
+wget http://download.geofabrik.de/osm/south-america/argentina.osm.pbf -O /home/apmon/planet_dumps/argentina.osm.pbf
+wget http://download.geofabrik.de/osm/north-america/canada/ontario.osm.pbf -O /home/apmon/planet_dumps/ontario.osm.pbf
+wget http://download.geofabrik.de/osm/north-america/canada/british-columbia.osm.pbf -O /home/apmon/planet_dumps/britishcolumbia.osm.pbf
+wget http://download.geofabrik.de/osm/north-america/us/california.osm.pbf -O /home/apmon/planet_dumps/california.osm.pbf
+wget http://download.geofabrik.de/osm/north-america/us/louisiana.osm.pbf -O /home/apmon/planet_dumps/louisiana.osm.pbf
+wget http://download.geofabrik.de/osm/north-america/us/colorado.osm.pbf -O /home/apmon/planet_dumps/colorado.osm.pbf
+wget http://download.geofabrik.de/osm/north-america/us/new-york.osm.pbf -O /home/apmon/planet_dumps/new_york.osm.pbf
+wget http://download.cloudmade.com/americas/caribbean/jamaica/jamaica.osm.bz2 -O /home/apmon/planet_dumps/jamaica.osm.bz2
+wget http://download.cloudmade.com/americas/northern_america/mexico/mexico.osm.bz2 -O /home/apmon/planet_dumps/mexico.osm.bz2
+wget http://download.cloudmade.com/americas/south_america/venezuela/venezuela.osm.bz2 -O /home/apmon/planet_dumps/venezuela.osm.bz2
+wget http://downloads.cloudmade.com/americas/caribbean/puerto_rico/puerto_rico.osm.bz2 -O /home/apmon/planet_dumps/puerto_rico.osm.bz2
+wget http://download.cloudmade.com/americas/caribbean/trinidad_and_tobago/trinidad_and_tobago.osm.bz2 -O /home/apmon/planet_dumps/trinidad.osm.bz2
+
+# Asia
+wget http://download.geofabrik.de/osm/asia/china.osm.pbf -O /home/apmon/planet_dumps/china.osm.pbf
+wget http://download.geofabrik.de/osm/asia/japan.osm.pbf -O /home/apmon/planet_dumps/japan.osm.pbf
+wget http://download.geofabrik.de/osm/asia/india.osm.pbf -O /home/apmon/planet_dumps/india.osm.pbf
+wget http://download.geofabrik.de/osm/asia/indonesia.osm.pbf -O /home/apmon/planet_dumps/indonesia.osm.pbf
+wget http://download.geofabrik.de/osm/asia/israel_and_palestine.osm.pbf -O /home/apmon/planet_dumps/israel.osm.pbf
+wget http://download.geofabrik.de/osm/asia/pakistan.osm.pbf -O /home/apmon/planet_dumps/pakistan.osm.pbf
+wget http://download.geofabrik.de/osm/asia/philippines.osm.pbf -O /home/apmon/planet_dumps/philippines.osm.pbf
+wget http://download.geofabrik.de/osm/asia/iraq.osm.pbf -O /home/apmon/planet_dumps/iraq.osm.pbf
+wget http://download.geofabrik.de/osm/asia/kazakhstan.osm.pbf -O /home/apmon/planet_dumps/kazakhstan.osm.pbf
+wget http://downloads.cloudmade.com/asia/south-eastern_asia/malaysia/malaysia.osm.bz2 -O /home/apmon/planet_dumps/malaysia.osm.bz2
+wget http://downloads.cloudmade.com/asia/south-eastern_asia/thailand/thailand.osm.bz2 -O /home/apmon/planet_dumps/thailand.osm.bz2
+wget http://download.cloudmade.com/asia/southern_asia/afghanistan/afghanistan.osm.bz2 -O /home/apmon/planet_dumps/afghanistan.osm.bz2
+wget http://download.cloudmade.com/asia/southern_asia/bangladesh/bangladesh.osm.bz2 -O /home/apmon/planet_dumps/bangladesh.osm.bz2
+wget http://downloads.cloudmade.com/asia/southern_asia/iran/iran.osm.bz2 -O /home/apmon/planet_dumps/iran.osm.bz2
+wget http://downloads.cloudmade.com/asia/southern_asia/sri_lanka/sri_lanka.osm.bz2 -O /home/apmon/planet_dumps/sri_lanka.osm.bz2
+wget http://downloads.cloudmade.com/asia/western_asia/saudi_arabia/saudi_arabia.osm.bz2 -O /home/apmon/planet_dumps/saudi_arabia.osm.bz2
+wget http://downloads.cloudmade.com/asia/western_asia/kuwait/kuwait.osm.bz2 -O /home/apmon/planet_dumps/kuwait.osm.bz2
+wget http://downloads.cloudmade.com/asia/eastern_asia/south_korea/south_korea.osm.bz2 -O /home/apmon/planet_dumps/south_korea.osm.bz2
+wget http://download.cloudmade.com/asia/western_asia/lebanon/lebanon.osm.bz2 -O /home/apmon/planet_dumps/lebanon.osm.bz2
+wget http://download.cloudmade.com/asia/south-eastern_asia/singapore/singapore.osm.bz2 -O /home/apmon/planet_dumps/singapore.osm.bz2
+wget http://download.cloudmade.com/asia/western_asia/jordan/jordan.osm.bz2 -O /home/apmon/planet_dumps/jordan.osm.bz2
+wget http://download.cloudmade.com/asia/western_asia/syria/syria.osm.bz2 -O /home/apmon/planet_dumps/syria.osm.bz2
+wget http://download.cloudmade.com/asia/western_asia/bahrain/bahrain.osm.bz2 -O /home/apmon/planet_dumps/bahrain.osm.bz2
+wget http://download.cloudmade.com/asia/south-eastern_asia/brunei/brunei.osm.bz2 -O /home/apmon/planet_dumps/brunei.osm.bz2
+wget http://download.cloudmade.com/asia/western_asia/united_arab_emirates/united_arab_emirates.osm.bz2  -O /home/apmon/planet_dumps/uae.osm.bz2
+wget http://downloads.cloudmade.com/asia/western_asia/georgia/georgia.osm.bz2 -O /home/apmon/planet_dumps/georgia.osm.bz2
+wget http://downloads.cloudmade.com/asia/western_asia/armenia/armenia.osm.bz2 -O /home/apmon/planet_dumps/armenia.osm.bz2
+wget http://downloads.cloudmade.com/asia/south-eastern_asia/myanmar/myanmar.osm.bz2 -O /home/apmon/planet_dumps/myanmar.osm.bz2
+wget http://downloads.cloudmade.com/asia/northern_asia/russia/russia.osm.bz2 -O /home/apmon/planet_dumps/russia.osm.bz2
+
+# Australia
+wget http://download.geofabrik.de/osm/australia-oceania.osm.pbf -O /home/apmon/planet_dumps/australia-oceania.osm.pbf
+
+# Europe
 wget http://download.geofabrik.de/osm/europe/malta.osm.pbf -O /home/apmon/planet_dumps/malta.osm.pbf
 wget http://download.geofabrik.de/osm/europe/austria.osm.pbf -O /home/apmon/planet_dumps/austria.osm.pbf
 wget http://download.geofabrik.de/osm/europe/poland.osm.pbf -O /home/apmon/planet_dumps/poland.osm.pbf
@@ -35,7 +102,9 @@ wget http://download.geofabrik.de/osm/europe/greece.osm.pbf -O /home/apmon/plane
 wget http://download.geofabrik.de/osm/europe/denmark.osm.pbf -O /home/apmon/planet_dumps/denmark.osm.pbf
 wget http://download.geofabrik.de/osm/europe/iceland.osm.bz2 -O /home/apmon/planet_dumps/iceland.osm.bz2
 wget http://download.geofabrik.de/osm/europe/italy.osm.pbf -O /home/apmon/planet_dumps/italy.osm.pbf
+wget http://download.geofabrik.de/osm/europe/portugal.osm.pbf -O /home/apmon/planet_dumps/portugal.osm.pbf
 wget http://download.geofabrik.de/osm/europe/romania.osm.pbf -O /home/apmon/planet_dumps/romania.osm.pbf
+wget http://download.geofabrik.de/osm/europe/russia-european-part.osm.pbf -O /home/apmon/planet_dumps/russia_europe.osm.pbf
 wget http://download.geofabrik.de/osm/europe/cyprus.osm.bz2 -O /home/apmon/planet_dumps/cyprus.osm.bz2
 wget http://download.geofabrik.de/osm/europe/hungary.osm.pbf -O /home/apmon/planet_dumps/hungary.osm.pbf
 wget http://download.geofabrik.de/osm/europe/ireland.osm.pbf -O /home/apmon/planet_dumps/ireland.osm.pbf
@@ -44,66 +113,3 @@ wget http://download.geofabrik.de/osm/europe/ukraine.osm.pbf -O /home/apmon/plan
 wget http://download.geofabrik.de/osm/europe/finland.osm.pbf -O /home/apmon/planet_dumps/finland.osm.pbf
 wget http://download.geofabrik.de/osm/europe/sweden.osm.pbf -O /home/apmon/planet_dumps/sweden.osm.pbf
 wget http://download.geofabrik.de/osm/europe/serbia.osm.pbf -O /home/apmon/planet_dumps/serbia.osm.pbf
-wget http://download.geofabrik.de/osm/africa/libya.osm.pbf -O /home/apmon/planet_dumps/libya.osm.pbf
-
-wget http://download.geofabrik.de/osm/asia/china.osm.pbf -O /home/apmon/planet_dumps/china.osm.pbf
-wget http://download.geofabrik.de/osm/asia/japan.osm.pbf -O /home/apmon/planet_dumps/japan.osm.pbf
-wget http://download.geofabrik.de/osm/asia/india.osm.pbf -O /home/apmon/planet_dumps/india.osm.pbf
-wget http://download.geofabrik.de/osm/asia/indonesia.osm.pbf -O /home/apmon/planet_dumps/indonesia.osm.pbf
-wget http://download.geofabrik.de/osm/asia/israel_and_palestine.osm.pbf -O /home/apmon/planet_dumps/israel.osm.pbf
-wget http://download.geofabrik.de/osm/asia/pakistan.osm.pbf -O /home/apmon/planet_dumps/pakistan.osm.pbf
-wget http://download.geofabrik.de/osm/asia/philippines.osm.pbf -O /home/apmon/planet_dumps/philippines.osm.pbf
-wget http://download.geofabrik.de/osm/asia/iraq.osm.pbf -O /home/apmon/planet_dumps/iraq.osm.pbf
-wget http://download.geofabrik.de/osm/asia/kazakhstan.osm.pbf -O /home/apmon/planet_dumps/kazakhstan.osm.pbf
-wget http://download.geofabrik.de/osm/africa/canary_islands.osm.pbf -O /home/apmon/planet_dumps/canary.osm.pbf
-wget http://download.geofabrik.de/osm/africa/south_africa_and_lesotho.osm.pbf -O /home/apmon/planet_dumps/south_africa.osm.pbf
-wget http://download.geofabrik.de/osm/central-america/haiti-and-domrep.osm.pbf -O /home/apmon/planet_dumps/haiti.osm.pbf
-wget http://download.geofabrik.de/osm/south-america/brazil.osm.pbf -O /home/apmon/planet_dumps/brazil.osm.pbf
-wget http://download.geofabrik.de/osm/south-america/colombia.osm.pbf -O /home/apmon/planet_dumps/colombia.osm.pbf
-wget http://download.geofabrik.de/osm/south-america/argentina.osm.pbf -O /home/apmon/planet_dumps/argentina.osm.pbf
-
-wget http://download.geofabrik.de/osm/north-america/canada/ontario.osm.pbf -O /home/apmon/planet_dumps/ontario.osm.pbf
-wget http://download.geofabrik.de/osm/north-america/canada/british-columbia.osm.pbf -O /home/apmon/planet_dumps/britishcolumbia.osm.pbf
-wget http://download.cloudmade.com/americas/caribbean/jamaica/jamaica.osm.bz2 -O /home/apmon/planet_dumps/jamaica.osm.bz2
-wget http://downloads.cloudmade.com/asia/southern_asia/iran/iran.osm.bz2 -O /home/apmon/planet_dumps/iran.osm.bz2
-wget http://downloads.cloudmade.com/asia/southern_asia/sri_lanka/sri_lanka.osm.bz2 -O /home/apmon/planet_dumps/sri_lanka.osm.bz2
-wget http://downloads.cloudmade.com/asia/western_asia/saudi_arabia/saudi_arabia.osm.bz2 -O /home/apmon/planet_dumps/saudi_arabia.osm.bz2
-wget http://downloads.cloudmade.com/asia/western_asia/kuwait/kuwait.osm.bz2 -O /home/apmon/planet_dumps/kuwait.osm.bz2
-wget http://downloads.cloudmade.com/asia/eastern_asia/south_korea/south_korea.osm.bz2 -O /home/apmon/planet_dumps/south_korea.osm.bz2
-wget http://downloads.cloudmade.com/africa/eastern_africa/zimbabwe/zimbabwe.osm.bz2 -O /home/apmon/planet_dumps/zimbabwe.osm.bz2
-wget http://downloads.cloudmade.com/africa/eastern_africa/ethiopia/ethiopia.osm.bz2 -O /home/apmon/planet_dumps/ethiopia.osm.bz2
-wget http://downloads.cloudmade.com/africa/western_africa/nigeria/nigeria.osm.bz2 -O /home/apmon/planet_dumps/nigeria.osm.bz2
-wget http://downloads.cloudmade.com/africa/northern_africa/egypt/egypt.osm.bz2 -O /home/apmon/planet_dumps/egypt.osm.bz2
-wget http://downloads.cloudmade.com/africa/northern_africa/morocco/morocco.osm.bz2 -O /home/apmon/planet_dumps/morocco.osm.bz2
-wget http://downloads.cloudmade.com/africa/middle_africa/democratic_republic_of_the_congo/democratic_republic_of_the_congo.osm.bz2 -O /home/apmon/planet_dumps/congo.osm.bz2
-wget http://downloads.cloudmade.com/africa/eastern_africa/kenya/kenya.osm.bz2 -O /home/apmon/planet_dumps/kenya.osm.bz2
-wget http://downloads.cloudmade.com/africa/eastern_africa/seychelles/seychelles.osm.bz2 -O /home/apmon/planet_dumps/seychelles.osm.bz2
-wget http://downloads.cloudmade.com/africa/southern_africa/namibia/namibia.osm.bz2 -O /home/apmon/planet_dumps/namibia.osm.bz2
-wget http://download.geofabrik.de/osm/europe/portugal.osm.pbf -O /home/apmon/planet_dumps/portugal.osm.pbf
-wget http://downloads.cloudmade.com/africa/northern_africa/tunisia/tunisia.osm.bz2 -O /home/apmon/planet_dumps/tunesia.osm.bz2
-wget http://download.geofabrik.de/osm/australia-oceania.osm.pbf -O /home/apmon/planet_dumps/australia-oceania.osm.pbf
-wget http://downloads.cloudmade.com/asia/northern_asia/russia/russia.osm.bz2 -O /home/apmon/planet_dumps/russia.osm.bz2
-wget http://download.geofabrik.de/osm/europe/russia-european-part.osm.pbf -O /home/apmon/planet_dumps/russia_europe.osm.pbf
-wget http://downloads.cloudmade.com/asia/south-eastern_asia/malaysia/malaysia.osm.bz2 -O /home/apmon/planet_dumps/malaysia.osm.bz2
-wget http://downloads.cloudmade.com/asia/south-eastern_asia/thailand/thailand.osm.bz2 -O /home/apmon/planet_dumps/thailand.osm.bz2
-wget http://download.cloudmade.com/asia/southern_asia/afghanistan/afghanistan.osm.bz2 -O /home/apmon/planet_dumps/afghanistan.osm.bz2
-wget http://download.cloudmade.com/asia/southern_asia/bangladesh/bangladesh.osm.bz2 -O /home/apmon/planet_dumps/bangladesh.osm.bz2
-wget http://download.geofabrik.de/osm/north-america/us/california.osm.pbf -O /home/apmon/planet_dumps/california.osm.pbf
-wget http://downloads.cloudmade.com/americas/caribbean/puerto_rico/puerto_rico.osm.bz2 -O /home/apmon/planet_dumps/puerto_rico.osm.bz2
-wget http://download.geofabrik.de/osm/north-america/us/louisiana.osm.pbf -O /home/apmon/planet_dumps/louisiana.osm.pbf
-wget http://download.geofabrik.de/osm/north-america/us/colorado.osm.pbf -O /home/apmon/planet_dumps/colorado.osm.pbf
-wget http://download.geofabrik.de/osm/north-america/us/new-york.osm.pbf -O /home/apmon/planet_dumps/new_york.osm.pbf
-wget http://download.cloudmade.com/americas/caribbean/trinidad_and_tobago/trinidad_and_tobago.osm.bz2 -O /home/apmon/planet_dumps/trinidad.osm.bz2
-
-wget http://download.cloudmade.com/americas/northern_america/mexico/mexico.osm.bz2 -O /home/apmon/planet_dumps/mexico.osm.bz2
-wget http://download.cloudmade.com/americas/south_america/venezuela/venezuela.osm.bz2 -O /home/apmon/planet_dumps/venezuela.osm.bz2
-wget http://download.cloudmade.com/asia/western_asia/lebanon/lebanon.osm.bz2 -O /home/apmon/planet_dumps/lebanon.osm.bz2
-wget http://download.cloudmade.com/asia/south-eastern_asia/singapore/singapore.osm.bz2 -O /home/apmon/planet_dumps/singapore.osm.bz2
-wget http://download.cloudmade.com/asia/western_asia/jordan/jordan.osm.bz2 -O /home/apmon/planet_dumps/jordan.osm.bz2
-wget http://download.cloudmade.com/asia/western_asia/syria/syria.osm.bz2 -O /home/apmon/planet_dumps/syria.osm.bz2
-wget http://download.cloudmade.com/asia/western_asia/bahrain/bahrain.osm.bz2 -O /home/apmon/planet_dumps/bahrain.osm.bz2
-wget http://download.cloudmade.com/asia/south-eastern_asia/brunei/brunei.osm.bz2 -O /home/apmon/planet_dumps/brunei.osm.bz2
-wget http://download.cloudmade.com/asia/western_asia/united_arab_emirates/united_arab_emirates.osm.bz2  -O /home/apmon/planet_dumps/uae.osm.bz2
-wget http://downloads.cloudmade.com/asia/western_asia/georgia/georgia.osm.bz2 -O /home/apmon/planet_dumps/georgia.osm.bz2
-wget http://downloads.cloudmade.com/asia/western_asia/armenia/armenia.osm.bz2 -O /home/apmon/planet_dumps/armenia.osm.bz2
-wget http://downloads.cloudmade.com/asia/south-eastern_asia/myanmar/myanmar.osm.bz2 -O /home/apmon/planet_dumps/myanmar.osm.bz2
