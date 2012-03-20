91fefb603f9e14dcb32d8eab8d3b4c56266279e7
jkpj <jkpj@users.sourceforge.net>
2012-03-20 14:00:00 +0200

    android: enable back key for tacho

--- src/de/ueller/gpsmid/ui/GuiTacho.java
+++ src/de/ueller/gpsmid/ui/GuiTacho.java
@@ -25,6 +25,10 @@ import de.ueller.util.Logger;
 
 import de.enough.polish.util.Locale;
 
+//#if polish.android
+import android.view.KeyEvent;
+//#endif
+
 /** 
  * Implements the "Tacho" screen which displays numbers such as speed, height etc.
  */
@@ -271,4 +275,15 @@ public class GuiTacho extends KeyCommandCanvas implements CommandListener,
 		repaint();
 	}
 
+	//#if polish.android
+	// not in keyPressed() to solve the problem that Back gets passed on
+	// to the next menu
+	// See http://developer.android.com/sdk/android-2.0.html
+	// for a possible Native Android workaround
+	protected void keyReleased(int keyCode) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			commandAction(BACK_CMD, (Displayable) null);
+		}
+	}
+	//#endif
 }
