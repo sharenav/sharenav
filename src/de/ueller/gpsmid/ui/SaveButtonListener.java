f64c4e4d93a5263848530c062d043f6270be3566
jkpj <jkpj@users.sourceforge.net>
2012-04-14 12:54:41 +0300

    android: save waypoint: refactor native android save button

---
+++ src/de/ueller/gpsmid/ui/SaveButtonListener.java
@@ -0,0 +1,9 @@
+package de.ueller.gpsmid.ui;
+
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Displayable;
+
+public interface SaveButtonListener {
+	public void commandAction(Command c, Displayable d);
+}
