1268190121f0d01cd41532b70e92497cd7f434aa
jkpj <jkpj@users.sourceforge.net>
2012-04-19 22:13:48 +0300

    fix clickable marker placing

--- src/de/ueller/gpsmid/tile/SingleTile.java
+++ src/de/ueller/gpsmid/tile/SingleTile.java
@@ -472,7 +472,7 @@ public class SingleTile extends Tile implements QueueableTile {
 					// FIXME create a specific color (semi-transparent would be good) for this
 					pc.g.setColor(Legend.COLORS[Legend.COLOR_ROUTE_ROUTELINE]);
 					pc.g.drawArc(pc.swapLineP.x - dia / 2, pc.swapLineP.y -
-						     (Legend.isNodeImageCentered(t) ? dia : dia / 2), dia, dia, 0, 360);
+						     (Legend.isNodeImageCentered(t) ? dia / 2 : dia / 2 + img.getHeight() / 2 ), dia, dia, 0, 360);
 					pc.trace.addClickableMarker(pc.swapLineP.x, pc.swapLineP.y, url);
 				}
 			}
