39b5a047f34f4799275cfedc8217d99d1e287363
sk750 <sk750@users.sourceforge.net>
2012-04-28 09:15:03 +0200

    refactor ImageCache to not use instanceOf anymore

--- src/de/ueller/midlet/util/ImageCache.java
+++ src/de/ueller/midlet/util/ImageCache.java
@@ -6,65 +6,119 @@ package de.ueller.midlet.util;
 
 import javax.microedition.lcdui.Image;
 import java.io.IOException;
+import java.lang.Integer;
 import java.util.Vector;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
+import de.ueller.gps.location.GetCompass;
 import de.ueller.util.Logger;
 
 import de.enough.polish.util.Locale;
 
 class ImgId {
-	String fileName = null;
-	Image baseImage = null;
-	int oneColor = 0;
 	short width;
 	short height;
 	
-	public ImgId(String fileName, int width, int height) {
+	public ImgId(int width, int height) {
 		this.width = (short) width;
 		this.height = (short) height;
-		this.fileName = fileName;
-	}
-
-	public ImgId(Image baseImage, int width, int height) {
-		this.width = (short) width;
-		this.height = (short) height;
-		this.baseImage = baseImage;
-	}
-
-	public ImgId(int oneColor, int width, int height) {
-		this.width = (short) width;
-		this.height = (short) height;
-		this.oneColor = oneColor;
 	}
 	
 	public boolean equals(Object o){
-		if (o == null || !(o instanceof ImgId) ) {
+		if (o == null || !(o instanceof ImgId) || o.getClass() != this.getClass()) {
 			return false;			
 		}
 		ImgId other = (ImgId) o;
-		if (other.width != this.width || other.height != this.height || other.baseImage != this.baseImage || other.oneColor != this.oneColor
-			||
-			(other.fileName != null && this.fileName != null && !other.fileName.equalsIgnoreCase(this.fileName))
-		) {
+		if (other.width != this.width || other.height != this.height) {
 			return false;
 		}
 		return true;
 	}	
 	
 	public int hashCode() {
-		return this.width + this.height * 256 + ((this.fileName == null) ? (this.baseImage == null ? this.oneColor : baseImage.hashCode()) : this.fileName.hashCode()); 		
+		return this.width + this.height; 		
 	}
-	
-	public String toString() {
-		if (this.fileName == null && this.baseImage == null) {
-			return "oneColor(" + this.width + "x" + this.height + ")";
-		}
-		return this.fileName == null ? super.toString() : this.fileName;
-	}
-	
+		
 }
+
+class ImgIdFile extends ImgId {
+	String fileName;
+	public ImgIdFile(String fileName, int width, int height) {
+		super(width, height);
+		this.fileName = fileName;
+	}
+
+	public boolean equals(Object o){
+		if (super.equals(o)) {
+			ImgIdFile other = (ImgIdFile) o;
+			if (this.fileName.equalsIgnoreCase(other.fileName)) {
+				return true;
+			}
+		}
+		return false;
+	}	
+	
+	public int hashCode() {
+		return super.hashCode() + fileName.hashCode(); 		
+	}
+
+	public String toString() {
+		return this.fileName;
+	}
+
+}
+
+class ImgIdOneColor extends ImgId {	
+	int color;
+
+	public ImgIdOneColor(int color, int width, int height) {
+		super(width, height);
+		this.color = color;
+	}
+
+	public boolean equals(Object o){
+		if (super.equals(o)) {
+			ImgIdOneColor other = (ImgIdOneColor) o;
+			if (this.color == other.color) {
+				return true;
+			}
+		}
+		return false;
+	}	
+	
+	public int hashCode() {
+		return super.hashCode() + this.color; 		
+	}
+
+	public String toString() {
+		return "oneColor(" + this.width + "x" + this.height + ")";
+	}
+}
+
+class ImgIdBaseImage extends ImgId {
+	Image baseImage;
+	
+	public ImgIdBaseImage(Image baseImage, int width, int height) {
+		super(width, height);
+		this.baseImage = baseImage;
+	}
+
+	public boolean equals(Object o){
+		if (super.equals(o)) {
+			ImgIdBaseImage other = (ImgIdBaseImage) o;
+			if (other.baseImage == this.baseImage) {
+				return true;
+			}
+		}
+		return false;
+	}	
+	
+	public int hashCode() {
+		return super.hashCode() + this.baseImage.hashCode(); 		
+	}
+}
+
 
 
 class CacheEntry {
@@ -85,80 +139,91 @@ public class ImageCache {
 
 	
 	/** returns an Image object of the given filename in its original width and height, either by loading it or taking it from the cache */
-	public static Image getImage(String fileName) {
-		return getImage(fileName, 0, 0);
+	public static Image getImageFromFile(String fileName) {
+		return getImageFromFile(fileName, 0, 0);
 	}
 	
 	/** returns an Image object of the given filename in the given width and height, either by loading it or taking it from the cache */
-	public static Image getImage(String fileName, int width, int height) {
-		return getImage(fileName, null, 0, width, height);
+	public static Image getImageFromFile(String fileName, int width, int height) {
+		Image img;
+		ImgIdFile id = new ImgIdFile(fileName, width, height);
+		CacheEntry cacheEntry = (CacheEntry) imageCache.get(id);
+		if (cacheEntry != null) {
+			return getCachedImage(cacheEntry);
+		}
+		// file system image
+		try {
+			img = Image.createImage(fileName);
+			System.out.println("Caching " + id.toString());				
+		} catch (IOException ioe) {
+			logger.error("Cannot load " + fileName);
+			return null;
+		}
+		if (width != 0) {
+			img = ImageTools.scaleImage(img, width, height);
+		}
+		if (img != null) {
+			System.out.println("Caching " + id.toString());				
+			imageCache.put(id, new CacheEntry(img));
+		}
+		return img;				
 	}
 
 	/** returns an Image object of the given base image in the given width and height, either by resizing it or taking it from the cache */
-	public static Image getScaledImage(Image baseImg, int width, int height) {
-		return getImage(null, baseImg, 0, width, height);
+	public static Image getScaledImage(Image baseImage, int width, int height) {
+		Image img = null;
+		ImgIdBaseImage id = new ImgIdBaseImage(baseImage, width, height);
+		CacheEntry cacheEntry = (CacheEntry) imageCache.get(id);
+		if (cacheEntry != null) {
+			return getCachedImage(cacheEntry);
+		}
+		if (width != 0) {
+			img = ImageTools.scaleImage(baseImage, width, height);
+		}
+		if (img != null) {
+			System.out.println("Caching " + id.toString());				
+			imageCache.put(id, new CacheEntry(img));
+		}
+		return img;				
 	}
 
 	/** returns an Image object of the given one color in the given width and height, either by creating it or taking it from the cache */
-	public static Image getOneColorImage(int oneColor, int width, int height) {
-		return getImage(null, null, oneColor, width, height);
+	public static Image getOneColorImage(int color, int width, int height) {
+		cleanup();
+		Image img;
+		ImgIdOneColor id = new ImgIdOneColor(color, width, height);
+		CacheEntry cacheEntry = (CacheEntry) imageCache.get(id);
+		if (cacheEntry != null) {
+			return getCachedImage(cacheEntry);
+		}
+		int rawSize = width * height;
+		int[] rawOutput = new int[rawSize];
+        for (int outOffset = 0; outOffset < rawSize;) {
+            rawOutput[outOffset++]= color;
+        }
+	    try {
+	        img = Image.createRGBImage(rawOutput, width, height, true);
+        } catch (Exception e) {
+			logger.exception("Cannot create one color image", e);
+			return null;
+        }
+		System.out.println("Caching " + id.toString());
+		imageCache.put(id, new CacheEntry(img));
+		return img;
 	}
 	
-	/** returns an Image object of the given parameters in the given width and height, either by loading / resizing / creating it or taking it from the cache */
-	public static Image getImage(String fileName, Image baseImg, int oneColor, int width, int height) {
+	public static Image getCachedImage(CacheEntry cacheEntry) {
+		cacheEntry.lastUsedTime = System.currentTimeMillis();
+		//System.out.println("Loaded from cache " + id.toString());
+		return cacheEntry.img;
+	}
+	
+	public static void cleanup() {
 		long now = System.currentTimeMillis(); 
 		if (Math.abs(now - lastCleanup) > 30000) {
 			cleanup(30000);
-		}
-		
-		ImgId id = (baseImg == null) ?
-				(fileName == null ? new ImgId(oneColor, width, height) : new ImgId(fileName, width, height))
-				:
-				new ImgId(baseImg, width, height);
-		Image img = null;
-		CacheEntry cacheEntry = (CacheEntry) imageCache.get(id);
-		if (cacheEntry == null) {
-			if (baseImg == null) {
-				if (fileName != null) {
-					// file system image
-					try {
-						baseImg = Image.createImage(fileName);
-						System.out.println("Loaded " + id.toString());				
-					} catch (IOException ioe) {
-						logger.error("Cannot load " + fileName);
-					}
-				}
-			}
-			// one color image
-			if (fileName == null && baseImg == null) {
-				int rawSize = width * height;
-				int[] rawOutput = new int[rawSize];        
-		        for (int outOffset = 0; outOffset < rawSize;) {
-		            rawOutput[outOffset++]= oneColor;
-		        }
-			    try {
-			        img = Image.createRGBImage(rawOutput, width, height, true);
-					System.out.println("Created " + id.toString());
-		        } catch (Exception e) {
-					logger.exception("Cannot create one color image", e);
-		        }
-	        } else if (width != 0) {
-				img = ImageTools.scaleImage(baseImg, width, height);
-			}
-			if (img != null) {
-				imageCache.put(id, new CacheEntry(img));
-			}
-		}
-		else {
-			img = cacheEntry.img;
-			cacheEntry.lastUsedTime = System.currentTimeMillis();
-			//imageCache.put(id, cacheEntry);
-			System.out.println("Loaded from cache " + id.toString());
-		}
-		
-		return img;		
+		}	
 	}
-	
 	
 	/** uncache all cached images that have not been used longer than the given time */
 	public static void cleanup(int minUnusedMillis) {
@@ -183,12 +248,12 @@ public class ImageCache {
 	public static void testImageCache() {
 		Image img;
 		System.out.println("Testing ImageCache");
-		img = getImage("/bus.png", 20, 20);
-		img = getImage("/church.png", 20, 20);
-		img = getImage("/bus.png", 20, 20);
+		img = getImageFromFile("/bus.png", 20, 20);
+		img = getImageFromFile("/church.png", 20, 20);
+		img = getImageFromFile("/bus.png", 20, 20);
 		cleanup(0);
-		img = getImage("/bus.png", 20, 20);
-		img = getImage("/church.png", 20, 20);
-		img = getImage("/church.png", 20, 20);
+		img = getImageFromFile("/bus.png", 20, 20);
+		img = getImageFromFile("/church.png", 20, 20);
+		img = getImageFromFile("/church.png", 20, 20);
 	}
 }
