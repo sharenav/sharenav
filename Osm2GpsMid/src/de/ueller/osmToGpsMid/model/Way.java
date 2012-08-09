cc9af0664b20807a4f4491136c239b08de1dce1c
jkpj <jkpj@users.sourceforge.net>
2012-08-09 09:01:01 +0300

    syntax fix

--- Osm2GpsMid/src/de/ueller/osmToGpsMid/model/Way.java
+++ Osm2GpsMid/src/de/ueller/osmToGpsMid/model/Way.java
@@ -1103,7 +1103,7 @@ public class Way extends Entity implements Comparable<Way> {
 	}
 
 	public Node getFirstNodeWithoutPOIType() {
-		for (Node n : ((path != null ? path : trianglePath).getNodes()) {
+		for (Node n : ((path != null ? path : trianglePath).getNodes())) {
 			if (n.getType(config) == -1) {
 				return n;
 			}
