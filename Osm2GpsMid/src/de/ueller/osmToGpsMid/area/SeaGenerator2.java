4de3f68c72650083bd308eb3eb39524a87b9971a
jkpj <jkpj@users.sourceforge.net>
2012-08-11 15:42:08 +0300

    change seatile params

--- Osm2GpsMid/src/de/ueller/osmToGpsMid/area/SeaGenerator2.java
+++ Osm2GpsMid/src/de/ueller/osmToGpsMid/area/SeaGenerator2.java
@@ -117,9 +117,8 @@ public class SeaGenerator2 {
 		// use sea tiles: divide map area into separate parts for more efficient processing (experimental)
 		if (configuration.getUseSeaTiles()) {
 			// divide map area into parts; run sea multipolygon generation for each part
-			// try to have a sea tile be roughly 1km * 1km
-			final int latDivider = (int) MyMath.dist(sw, nw) / 80000 + 2;
-			final int lonDivider = (int) MyMath.dist(sw, se) / 80000 + 2;
+			final int latDivider = (int) MyMath.dist(sw, nw) / 5000 + 2;
+			final int lonDivider = (int) MyMath.dist(sw, se) / 5000 + 2;
 
 			Bounds allMapBounds = seaBounds.clone();
 
