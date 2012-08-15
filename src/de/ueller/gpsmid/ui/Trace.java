121e8b519b5a3e2d9029e87294fa872e31824f20
jkpj <jkpj@users.sourceforge.net>
2012-08-15 09:43:26 +0300

    map markers: enable long tap for tapping on marker, even when long tap on map is not on

--- src/de/ueller/gpsmid/ui/Trace.java
+++ src/de/ueller/gpsmid/ui/Trace.java
@@ -4297,7 +4297,7 @@ CompassReceiver, Runnable , GpsMidDisplayable, CompletionListener, IconActionPer
 	private void longTap(boolean force) {
 		// if not tapping a control, then the map area must be tapped so we do the long tap action for the map area
 		if (tl.getElementIdAtPointer(touchX, touchY) < 0 && panProjection != null) {							
-			if (!pointerDraggedMuch && (Configuration.getCfgBitState(Configuration.CFGBIT_MAPTAP_LONG) || force)) {
+			if (!pointerDraggedMuch && (Configuration.getCfgBitState(Configuration.CFGBIT_MAPTAP_LONG) || force || getClickableMarker(touchX, touchY) != null)) {
 				pointerActionDone = true;
 				//#debug debug
 				logger.debug("long tap map");										
