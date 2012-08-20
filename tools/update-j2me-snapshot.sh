6af56d33304e1def1be2f3b9d9ab8b5126d93449
jkpj <jkpj@users.sourceforge.net>
2012-08-20 20:57:12 +0300

    add nightly J2ME build script

---
+++ tools/update-j2me-snapshot.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+#
+# update the GpsMid "bare binaries" J2ME snapshots
+# FIXME: bundle a small map with the app
+#
+
+user=SOURCEFORGE_USERNAME_HERE
+. ./android.properties
+
+ant clean
+
+# workaround for the first i18n-messages failure
+#
+ant -Ddevice=Generic/blackberry
+
+
+# normal build 
+
+ant 
+
+#scp -p dist/*.apk $user,gpsmid@web.sf.net:htdocs/prebuild
+
+
+ssh $user,gpsmid@shell.sf.net create
+
+cd dist
+
+ln -f -s `ls -t *full-connected-outl*jar|head -1` GpsMid-latest.jar
+
+tar cf - GpsMid-Generic*.jar | ssh $user,gpsmid@shell.sf.net 'cd /home/project-web/gpsmid/htdocs/prebuild ; tar xpf -'
+
+# debug build
+
+cd ..
+
+ant clean
+
+# workaround for the first i18n-messages failure
+#
+ant -Ddevice=Generic/blackberry
+
+
+# normal build 
+
+ant  debug
+
+cd dist
+
+for i in *.jar
+do
+ mv $i `echo $i | sed 's/Generic-/Generic-debug-/'`
+done
+
+ln -f -s `ls -t *full-connected*jar|head -1` GpsMid-latest-debug.jar
+
+tar cf - *.apk | ssh $user,gpsmid@shell.sf.net 'cd /home/project-web/gpsmid/htdocs/prebuild ; tar xpf -'
