d170a882c37d29e4e357cee9ea0f2e82f21519c0
sk750 <sk750@users.sourceforge.net>
2012-08-22 19:45:40 +0200

    Unlocalize SMS format - fix spacing

--- src/de/ueller/gpsmid/ui/GuiSendMessage.java
+++ src/de/ueller/gpsmid/ui/GuiSendMessage.java
@@ -41,6 +41,7 @@ public class GuiSendMessage extends Form implements CommandListener {
 		try {
 			tfMessage = new TextField(Locale.get("guisendmessage.MessageText")/*Message text*/,
 								"Lat: " + tr.center.radlat * MoreMath.FAC_RADTODEC +
+								" " +
 								"Lon: " + tr.center.radlon * MoreMath.FAC_RADTODEC +
 //						        Locale.get("guisendmessage.Lat")/*Lat: */ + tr.center.radlat * MoreMath.FAC_RADTODEC +
 //						        Locale.get("guisendmessage.Lon")/* Lon: */ + tr.center.radlon * MoreMath.FAC_RADTODEC +
