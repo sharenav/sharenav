d9a7bca4fc10aa780c18060fe16df9d991eb7c99
Jyrki Kuoppala <jkpj@users.sourceforget.net>
2012-08-25 12:12:20 +0300

    update release creation script to include android

--- tools/release-build.sh
+++ tools/release-build.sh
@@ -3,11 +3,13 @@
 # build Osm2GpsMid & GpsMid midlets for signing
 #
 
-numver=0.7.6
+numver=0.8
 ver=$numver-map67
 map=Berlin
 
 ant clean
+( cd Osm2GpsMid ; ant clean )
+
 #
 # workaround for the first i18n-messages failure
 #
@@ -16,11 +18,22 @@ ant -Ddevice=Generic/blackberry
 # normal build 
 
 ant
+
+ant -propertyfile android.properties android
+
 cd Osm2GpsMid
-ant clean
+
+# ant clean
 ant
 
-for target in full editing blackberry minimal
+for target in full-connected full midsize minimal blackberry
+do
+  java -Xmx1024m -jar dist/Osm2GpsMid-$ver.jar --nogui --properties=mapdef/$map-$target
+#  mv GpsMid-$ver.jar GpsMid-Generic-$target-$ver.jar
+#  mv GpsMid-$ver.jad GpsMid-Generic-$target-$ver.jad
+done
+
+for target in full-connected full midsize minimal
 do
   java -Xmx1024m -jar dist/Osm2GpsMid-$ver.jar --nogui --properties=mapdef/$map-$target
 #  mv GpsMid-$ver.jar GpsMid-Generic-$target-$ver.jar
@@ -37,6 +50,7 @@ cp Osm2GpsMid/*-$ver.ja? README.mkd WHATSNEW.txt Osm2GpsMid-$ver.jar "Release $n
 ant clean
 ant debug -Ddevice=Generic/blackberry
 ant debug j2mepolish
+ant -propertyfile android.properties debug android
 
 cd Osm2GpsMid
 ant clean
