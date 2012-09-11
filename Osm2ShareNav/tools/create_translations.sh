6de7d795205519b59131baa7b44ee494a12aa20f
jkpj <jkpj@users.sourceforge.net>
2012-09-11 23:10:06 +0300

    add cs translations to Osm2ShareNav

--- Osm2ShareNav/tools/create_translations.sh
+++ Osm2ShareNav/tools/create_translations.sh
@@ -2,3 +2,4 @@
 
 # create translations for Osm2ShareNav
 msgfmt --properties-input -r Translations -l fi -d . --java2  resources/Translations_fi.properties
+msgfmt --properties-input -r Translations -l cs -d . --java2  resources/Translations_cs.properties
